01e96884e8cce8221d6fbcf02dd0c2570772fe87
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2010-09-18 20:39:05 +0000

    implemented more commands
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.1@128 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- server/controller/amcp/command.cpp
+++ server/controller/amcp/command.cpp
@@ -6,13 +6,87 @@
 #include "../../producer/media.h"
 
 #include "../../renderer/render_device.h"
+#include "../../frame/frame_format.h"
+
+#include "../../server.h"
 
 #include <boost/regex.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/range.hpp>
+#include <boost/date_time.hpp>
 
 namespace caspar { namespace controller { namespace amcp {
+		
+std::wstring list_media()
+{	
+	std::wstringstream replyString;
+	for (boost::filesystem::wrecursive_directory_iterator itr(server::media_folder()), end; itr != end; ++itr)
+    {			
+		if(boost::filesystem::is_regular_file(itr->path()))
+		{
+			std::wstring clipttype = TEXT(" N/A ");
+			std::wstring extension = boost::to_upper_copy(itr->path().extension());
+			if(extension == TEXT(".TGA") || extension == TEXT(".COL"))
+				clipttype = TEXT(" STILL ");
+			else if(extension == TEXT(".SWF") || extension == TEXT(".DV") || extension == TEXT(".MOV") || extension == TEXT(".MPG") || 
+					extension == TEXT(".AVI") || extension == TEXT(".FLV") || extension == TEXT(".F4V") || extension == TEXT(".MP4"))
+				clipttype = TEXT(" MOVIE ");
+			else if(extension == TEXT(".WAV") || extension == TEXT(".MP3"))
+				clipttype = TEXT(" STILL ");
+
+			if(clipttype != TEXT(" N/A "))
+			{		
+				auto is_not_digit = [](char c){ return std::isdigit(c) == 0; };
+
+				auto relativePath = boost::filesystem::wpath(itr->path().file_string().substr(server::media_folder().size(), itr->path().file_string().size()));
+
+				auto writeTimeStr = boost::posix_time::to_iso_string(boost::posix_time::from_time_t(boost::filesystem::last_write_time(itr->path())));
+				writeTimeStr.erase(std::remove_if(writeTimeStr.begin(), writeTimeStr.end(), is_not_digit), writeTimeStr.end());
+				auto writeTimeWStr = std::wstring(writeTimeStr.begin(), writeTimeStr.end());
+
+				auto sizeStr = boost::lexical_cast<std::wstring>(boost::filesystem::file_size(itr->path()));
+				sizeStr.erase(std::remove_if(sizeStr.begin(), sizeStr.end(), is_not_digit), sizeStr.end());
+				auto sizeWStr = std::wstring(sizeStr.begin(), sizeStr.end());
+
+				replyString << TEXT("\"") << relativePath.replace_extension(TEXT(""))
+							<< TEXT("\" ") << clipttype 
+							<< TEXT(" ") << sizeStr
+							<< TEXT(" ") << writeTimeWStr
+							<< TEXT("\r\n");
+			}	
+		}
+	}
+	return boost::to_upper_copy(replyString.str());
+}
+
+std::wstring list_templates() 
+{
+	std::wstringstream replyString;
+
+	for (boost::filesystem::wrecursive_directory_iterator itr(server::template_folder()), end; itr != end; ++itr)
+    {		
+		if(boost::filesystem::is_regular_file(itr->path()) && itr->path().extension() == L".ft")
+		{
+			auto relativePath = boost::filesystem::wpath(itr->path().file_string().substr(server::template_folder().size(), itr->path().file_string().size()));
+
+			auto writeTimeStr = boost::posix_time::to_iso_string(boost::posix_time::from_time_t(boost::filesystem::last_write_time(itr->path())));
+			writeTimeStr.erase(std::remove_if(writeTimeStr.begin(), writeTimeStr.end(), [](char c){ return std::isdigit(c) == 0;}), writeTimeStr.end());
+			auto writeTimeWStr = std::wstring(writeTimeStr.begin(), writeTimeStr.end());
+
+			auto sizeStr = boost::lexical_cast<std::string>(boost::filesystem::file_size(itr->path()));
+			sizeStr.erase(std::remove_if(sizeStr.begin(), sizeStr.end(), [](char c){ return std::isdigit(c) == 0;}), sizeStr.end());
+
+			auto sizeWStr = std::wstring(sizeStr.begin(), sizeStr.end());
 			
+			replyString << TEXT("\"") << relativePath.replace_extension(TEXT(""))
+						<< TEXT("\" ") << sizeWStr
+						<< TEXT(" ") << writeTimeWStr
+						<< TEXT("\r\n");		
+		}
+	}
+	return boost::to_upper_copy(replyString.str());
+}
+
 unsigned int get_channel_index(const std::wstring& index_str, const std::vector<renderer::render_device_ptr>& channels)
 {	
 	int channel_index = boost::lexical_cast<int>(index_str)-1;
@@ -24,12 +98,37 @@ unsigned int get_channel_index(const std::wstring& index_str, const std::vector<
 
 std::function<std::wstring()> channel_info_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bINFO\\b\\s?(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+
+	if(!what["CHANNEL"].matched)
+		BOOST_THROW_EXCEPTION(missing_parameter() << arg_name_info("CHANNEL"));
+	
+	int  channel_index	= get_channel_index(what["CHANNEL"].str(), channels);
+	auto channel		= channels[channel_index];
+	int	 layer_index	= what["LAYER"].matched ? boost::lexical_cast<int>(what["LAYER"].str()) : DEFAULT_CHANNEL_LAYER;
+
+	std::wstring index_str = boost::lexical_cast<std::wstring>(channel_index);
+	if(what["LAYER"].matched)
+		index_str += L"-" + boost::lexical_cast<std::wstring>(layer_index);
+
+	return [=]() -> std::wstring
+	{
+		std::wstringstream str;
+		str << index_str << TEXT(" ") << channel->frame_format_desc().name  << TEXT("\r\n") << (channel->active(layer_index) != nullptr ? TEXT(" PLAYING") : TEXT(" STOPPED"));;	
+		
+		CASPAR_LOG(info) << L"Executed [amcp_info]";
+		
+		return str.str();
+	};
 }
 
 std::function<std::wstring()> load_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	static boost::wregex expr(L"\\bLOAD(?<NOPREVIEW>BG)?\\s(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s(?<PARAMS>.*)");
+	static boost::wregex expr(L"\\bLOAD(?<NOPREVIEW>BG)?\\b\\s?(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s(?<PARAMS>.*)");
 
 	boost::wsmatch what;
 	if(!boost::regex_match(message, what, expr))
@@ -67,7 +166,7 @@ std::function<std::wstring()> load_command::parse(const std::wstring& message, c
 
 std::function<std::wstring()> play_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	static boost::wregex expr(L"\\bPLAY\\s(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
+	static boost::wregex expr(L"\\bPLAY\\b\\s?(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
 
 	boost::wsmatch what;
 	if(!boost::regex_match(message, what, expr))
@@ -92,7 +191,7 @@ std::function<std::wstring()> play_command::parse(const std::wstring& message, c
 
 std::function<std::wstring()> stop_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	static boost::wregex expr(L"\\bSTOP\\s(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
+	static boost::wregex expr(L"\\bSTOP\\b\\s?(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
 
 	boost::wsmatch what;
 	if(!boost::regex_match(message, what, expr))
@@ -117,7 +216,7 @@ std::function<std::wstring()> stop_command::parse(const std::wstring& message, c
 
 std::function<std::wstring()> clear_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	static boost::wregex expr(L"\\bCLEAR\\s(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
+	static boost::wregex expr(L"\\bCLEAR\\b\\s?(?<CHANNEL>\\d)?-?(?<LAYER>\\d)?\\s*");
 
 	boost::wsmatch what;
 	if(!boost::regex_match(message, what, expr))
@@ -142,27 +241,72 @@ std::function<std::wstring()> clear_command::parse(const std::wstring& message, 
 
 std::function<std::wstring()> list_media_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bCLS\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+	
+	return [=]() -> std::wstring
+	{
+		CASPAR_LOG(info) << L"Executed [amcp_list_media]";
+
+		return list_media();	
+	};
 }
 
 std::function<std::wstring()> list_template_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bTLS\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+	
+	return [=]() -> std::wstring
+	{
+		CASPAR_LOG(info) << L"Executed [amcp_list_template]";
+
+		return list_templates();	
+	};
 }
 
 std::function<std::wstring()> media_info_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bCINF\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+	
+	BOOST_THROW_EXCEPTION(not_implemented());
 }
 
 std::function<std::wstring()> template_info_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bTINF\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+	
+	BOOST_THROW_EXCEPTION(not_implemented());
 }
 
 std::function<std::wstring()> version_info_command::parse(const std::wstring& message, const std::vector<renderer::render_device_ptr>& channels)
 {
-	return nullptr;
+	static boost::wregex expr(L"\\bVERSION\\s*");
+
+	boost::wsmatch what;
+	if(!boost::regex_match(message, what, expr))
+		return nullptr;
+	
+	return [=]() -> std::wstring
+	{
+		CASPAR_LOG(info) << L"Executed [amcp_version_info]";
+
+		return std::wstring(TEXT(CASPAR_VERSION_STR)) + TEXT("\r\n");	
+	};	
 }
 
 }}}
--- server/controller/amcp/controller.h
+++ server/controller/amcp/controller.h
@@ -64,11 +64,12 @@ private:
 		{
 			auto func = command::parse(message, channels);
 			if(func != nullptr)
-				func();
+				reply = func();
 			return func != nullptr;
 		}
 		const std::wstring& message;
 		const std::vector<renderer::render_device_ptr>& channels;
+		std::wstring reply;
 	};
 
 	void on_next(const std::wstring& message, int tag)
@@ -76,36 +77,60 @@ private:
 		command_queue_.push([=]
 		{
 			unparsed_ += message;
-			std::vector<std::wstring> split;
-			boost::iter_split(split, unparsed_, boost::algorithm::first_finder(L"\r\n"));
-			unparsed_ = split.back();
-			for(size_t n = 0; n < split.size()-1; ++n)
+			std::vector<std::wstring> message_split;
+			boost::iter_split(message_split, unparsed_, boost::algorithm::first_finder(L"\r\n"));
+			unparsed_ = message_split.back();
+			for(size_t n = 0; n < message_split.size()-1; ++n)
 			{
 				try
 				{
-					command_executor executor = {split[n], channels_};
-					CASPAR_LOG(info) << "[amcp_controller] Received: " << split[n];
+					command_executor executor = {message_split[n], channels_};
+					CASPAR_LOG(info) << "[amcp_controller] Received: " << message_split[n];
 					if(!boost::fusion::any(command_list(), executor))
-						stream_.start_write("400\r\n", tag);
+						stream_.start_write("400 INVALID\r\n", tag);
+					else
+					{
+						std::vector<std::wstring> reply_split;
+						boost::iter_split(reply_split, executor.reply, boost::algorithm::first_finder(L"\r\n"));
+
+						if(reply_split.empty())
+							stream_.start_write("202 OK\r\n", tag);
+						else if(reply_split.size() == 1)
+						{
+							stream_.start_write("201 OK\r\n", tag);
+							stream_.start_write(common::narrow(reply_split[0]) + "\r\n", tag);
+						}
+						else
+						{
+							stream_.start_write("200 OK\r\n", tag);
+							for(size_t n = 0; n < reply_split.size(); ++n)
+								stream_.start_write(common::narrow(reply_split[n]) + "\r\n", tag);
+						}
+					}
 				}
 				catch(invalid_channel&)
 				{
-					stream_.start_write("401\r\n", tag);
+					stream_.start_write("401 INVALID\r\n", tag);
 					CASPAR_LOG_CURRENT_EXCEPTION();
 				}
 				catch(file_not_found&)
 				{
-					stream_.start_write("404\r\n", tag);
+					stream_.start_write("404 ERROR\r\n", tag);
 					CASPAR_LOG_CURRENT_EXCEPTION();
 				}
 				catch(file_read_error&)
 				{
-					stream_.start_write("502\r\n", tag);
+					stream_.start_write("502 FAILED\r\n", tag);
+					CASPAR_LOG_CURRENT_EXCEPTION();
+				}
+				catch(not_implemented&)
+				{
+					stream_.start_write("600 FAILED\r\n", tag);
 					CASPAR_LOG_CURRENT_EXCEPTION();
 				}
 				catch(...)
 				{
-					stream_.start_write("501\r\n", tag);
+					stream_.start_write("501 FAILED\r\n", tag);
 					CASPAR_LOG_CURRENT_EXCEPTION();
 				}
 			}
