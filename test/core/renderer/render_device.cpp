8ff4a7bfe2c39f89af241a51229dd8be446ac013
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2010-11-01 21:54:47 +0000

    2.0.0.2: - Missing files.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@194 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ test/core/renderer/render_device.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <core/frame/gpu_frame.h>
+#include <core/renderer/layer.h>
+#include <core/producer/frame_producer.h>
+#include <common/exception/exceptions.h>
+
+using namespace caspar;
+using namespace caspar::core;
