5731cde69e151185999a9a70c0e9f5c0125bd70d
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2010-11-28 00:45:16 +0000

    2.0.0.2:  - gpu bicubic scaling. git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@264 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/processor/frame_shader.cpp
+++ core/processor/frame_shader.cpp
@@ -89,6 +89,8 @@ public:
 	{	
 		GL(glUseProgramObjectARB(program_));		
 	}
+
+	GLuint program() { return program_; }
 	
 private:
 	GLuint program_;
@@ -120,13 +122,71 @@ struct frame_shader::implementation
 			"	color.a = a;														"
 			"																		"
 			"	return color;														"
-			"}																		";
+			"}																		"
+			"																		"
+			"vec4 texture2DBilinear(sampler2D sampler, vec2 uv, vec4 size)			"
+			"{																		"
+			"	vec4 x0 = texture2D(sampler, uv + size.zw * vec2(0.0, 0.0));		"
+			"	vec4 x1 = texture2D(sampler, uv + size.zw * vec2(1.0, 0.0));		"
+			"	vec4 x2 = texture2D(sampler, uv + size.zw * vec2(0.0, 1.0));		"
+			"	vec4 x3 = texture2D(sampler, uv + size.zw * vec2(1.0, 1.0));		"
+			"	vec2 f  = fract(uv.xy * size.xy );									"
+			"	vec4 y0 = mix(x0, x1, f.x);											"
+			"	vec4 y1 = mix(x2, x3, f.x);											"
+			"	return mix(y0, y1, f.y);											"
+			"}																		"
+			"																					"
+			"vec4 computeWeights(float x)														"
+			"{																					"
+			"	vec4 x1 = x*vec4(1.0, 1.0, -1.0, -1.0) + vec4(1.0, 0.0, 1.0, 2.0);				"
+			"	vec4 x2 = x1*x1;																"
+			"	vec4 x3 = x2*x1;																"
+			"																					"
+			"	const float A = -0.75;															"	
+			"	vec4 w;																			"
+			"	w =  x3 * vec2(  A,      A+2.0).xyyx;											"
+			"	w += x2 * vec2( -5.0*A, -(A+3.0)).xyyx;											"
+			"	w += x1 * vec2(  8.0*A,  0).xyyx;												"
+			"	w +=      vec2( -4.0*A,  1.0).xyyx;												"
+			"	return w;																		"
+			"}																					"
+			"																					"
+			"vec4 cubicFilter(vec4 w, vec4 c0, vec4 c1, vec4 c2, vec4 c3)						"
+			"{																					"
+			"	return c0*w[0] + c1*w[1] + c2*w[2] + c3*w[3];									"
+			"}																					"
+			"																					"
+			"vec4 texture2DBicubic(sampler2D sampler, vec2 uv, vec4 size)						"
+			"{																					"
+			"	vec2 f = fract(uv*size.xy);														"
+			"																					"
+			"	vec4 w = computeWeights(f.x);													"
+			"	vec4 t0 = cubicFilter(w,	texture2D(sampler, uv + vec2(-1.0, -1.0) * size.zw),"
+			"				  				texture2D(sampler, uv + vec2( 0.0, -1.0) * size.zw),"
+			"				  				texture2D(sampler, uv + vec2( 1.0, -1.0) * size.zw),"
+			"				  				texture2D(sampler, uv + vec2( 2.0, -1.0) * size.zw));"
+			"	vec4 t1 = cubicFilter(w,	texture2D(sampler, uv + vec2(-1.0,  0.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 0.0,  0.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 1.0,  0.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 2.0,  0.0) * size.zw));"
+			"	vec4 t2 = cubicFilter(w,	texture2D(sampler, uv + vec2(-1.0,  1.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 0.0,  1.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 1.0,  1.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 2.0,  1.0) * size.zw));"
+			"	vec4 t3 = cubicFilter(w,	texture2D(sampler, uv + vec2(-1.0,  2.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 0.0,  2.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 1.0,  2.0) * size.zw),"
+			"								texture2D(sampler, uv + vec2( 2.0,  2.0) * size.zw));"
+			"																					"
+			"	w = computeWeights(f.y);														"
+			"	return cubicFilter(w, t0, t1, t2, t3);											"
+			"}																					";
 			
 		shaders_[pixel_format::abgr] = common +
 
 			"void main()															"
 			"{																		"
-				"vec4 abgr = texture2D(plane[0], gl_TexCoord[0].st);				"
+			"	vec4 abgr = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]);"
 				"gl_FragColor = abgr.argb * gl_Color;								"
 			"}																		";
 		
@@ -134,7 +194,7 @@ struct frame_shader::implementation
 
 			"void main()															"	
 			"{																		"
-				"vec4 argb = texture2D(plane[0], gl_TexCoord[0].st);				"
+				"vec4 argb = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]);"
 				"gl_FragColor = argb.grab * gl_Color;								"	
 			"}																		";
 		
@@ -142,7 +202,7 @@ struct frame_shader::implementation
 
 			"void main()															"
 			"{																		"
-				"vec4 bgra = texture2D(plane[0], gl_TexCoord[0].st);				"
+				"vec4 bgra = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]);"
 				"gl_FragColor = bgra.rgba * gl_Color;								"
 			"}																		";
 		
@@ -150,7 +210,7 @@ struct frame_shader::implementation
 
 			"void main()															"
 			"{																		"
-				"vec4 rgba = texture2D(plane[0], gl_TexCoord[0].st);				"
+				"vec4 rgba = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]);"
 				"gl_FragColor = rgba.bgra * gl_Color;								"
 			"}																		";
 		
@@ -158,9 +218,9 @@ struct frame_shader::implementation
 
 			"void main()															"
 			"{																		"
-				"float y  = texture2D(plane[0], gl_TexCoord[0].st).r;				"
-				"float cb = texture2D(plane[1], gl_TexCoord[0].st).r;				"
-				"float cr = texture2D(plane[2], gl_TexCoord[0].st).r;				"
+				"float y  = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]).r;"
+				"float cb = texture2DBicubic(plane[1], gl_TexCoord[0].st+plane_size[1].zw*0.5, plane_size[1]).r;"
+				"float cr = texture2DBicubic(plane[2], gl_TexCoord[0].st+plane_size[2].zw*0.5, plane_size[2]).r;"
 				"float a = 1.0;														"	
 				"gl_FragColor = ycbcra_to_bgra(y, cb, cr, a) * gl_Color;			"
 			"}																		";
@@ -169,10 +229,10 @@ struct frame_shader::implementation
 
 			"void main()															"
 			"{																		"
-				"float y  = texture2D(plane[0], gl_TexCoord[0].st).r;				"
-				"float cb = texture2D(plane[1], gl_TexCoord[0].st).r;				"
-				"float cr = texture2D(plane[2], gl_TexCoord[0].st).r;				"
-				"float a  = texture2D(plane[3], gl_TexCoord[0].st).r;				"
+				"float y  = texture2DBicubic(plane[0], gl_TexCoord[0].st+plane_size[0].zw*0.5, plane_size[0]).r;"
+				"float cb = texture2DBicubic(plane[1], gl_TexCoord[0].st+plane_size[1].zw*0.5, plane_size[1]).r;"
+				"float cr = texture2DBicubic(plane[2], gl_TexCoord[0].st+plane_size[2].zw*0.5, plane_size[2]).r;"
+				"float a  = texture2DBicubic(plane[3], gl_TexCoord[0].st+plane_size[3].zw*0.5, plane_size[3]).r;"
 				"gl_FragColor = ycbcra_to_bgra(y, cb, cr, a) * gl_Color;			"
 			"}																		";
 	}
@@ -180,6 +240,7 @@ struct frame_shader::implementation
 	void use(const pixel_format_desc& desc)
 	{
 		set_pixel_format(desc.pix_fmt);
+		set_planes(desc.planes);
 	}
 
 	void set_pixel_format(pixel_format::type format)
@@ -188,6 +249,13 @@ struct frame_shader::implementation
 			return;
 		current_ = format;
 		shaders_[format].use();
+	}
+
+	void set_planes(const std::vector<pixel_format_desc::plane>& planes)
+	{
+		for(size_t n = 0; n < planes.size(); ++n)
+			glUniform4f(glGetUniformLocation(shaders_[current_].program(), std::string("plane_size[" + boost::lexical_cast<std::string>(n) + "]").c_str()), 
+				static_cast<float>(planes[n].width), static_cast<float>(planes[n].height), 1.0f/static_cast<float>(planes[n].width), 1.0f/static_cast<float>(planes[n].height));
 	}
 
 	//void set_size(pixel_format_desc& desc)
