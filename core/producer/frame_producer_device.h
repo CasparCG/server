48d56be4a65df147c7693344f56ad93260df560a
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-01-17 18:02:40 +0000

    2.0.0.2: Added missing files git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@379 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ core/producer/frame_producer_device.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "frame_producer.h"
+
+#include <common/memory/safe_ptr.h>
+
+#include <boost/noncopyable.hpp>
+#include <boost/thread/future.hpp>
+
+#include <functional>
+
+namespace caspar { namespace core {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \class	frame_producer_device
+///
+/// \brief
+/// 		
+///                |**********| <-   empty frame   <- |***********| <-   frame format  <- |**********|
+///   PROTOCOL ->  | PRODUCER |                       |   MIXER	  |                       | CONSUMER |  -> DISPLAY DEVICE
+///                |**********| -> rendered frames -> |***********| -> formatted frame -> |**********|
+///   
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class frame_producer_device : boost::noncopyable
+{
+public:
+	typedef std::function<void(const safe_ptr<draw_frame>&)> output_func;
+
+	explicit frame_producer_device(const video_format_desc& format_desc, const safe_ptr<frame_factory>& factory, const output_func& output);
+	frame_producer_device(frame_producer_device&& other);
+		
+	// Layers and Producers
+	void set_video_gain(int index, double value);
+	void set_video_opacity(int index, double value);
+
+	void set_audio_gain(int index, double value);
+
+	void load(int index, const safe_ptr<frame_producer>& producer, bool play_on_load = false);
+	void preview(int index, const safe_ptr<frame_producer>& producer);
+	void pause(int index);
+	void play(int index);
+	void stop(int index);
+	void clear(int index);
+	void clear();	
+	boost::unique_future<safe_ptr<frame_producer>> foreground(int index) const;
+	boost::unique_future<safe_ptr<frame_producer>> background(int index) const;
+	const video_format_desc& get_video_format_desc() const;
+
+private:
+	struct implementation;
+	safe_ptr<implementation> impl_;
+};
+
+}}
