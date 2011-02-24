60ae183f4c30c8fe53ffa5a9207f1bee230247d8
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-02-24 16:38:47 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@476 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- common/compiler/vs/disable_silly_warnings.h
+++ common/compiler/vs/disable_silly_warnings.h
@@ -4,4 +4,6 @@
 #pragma warning (disable : 4482) // nonstandard extension used: enum 'enum' used in qualified name
 #pragma warning (disable : 4127) // conditional expression is constant
 #pragma warning (disable : 4714) // marked as __forceinline not inlined
+#pragma warning (disable : 4503) //  decorated name length exceeded, name was truncated
 #endif
+
