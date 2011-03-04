8f8066b6d92b2424addbd142fa950378a847a048
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-03-04 13:51:48 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@520 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- common/diagnostics/graph.h
+++ common/diagnostics/graph.h
@@ -5,6 +5,11 @@
 #include "../utility/printer.h"
 
 #include <string>
+#include <vector>
+#include <map>
+
+#include <boost/range/iterator_range.hpp>
+#include <boost/circular_buffer.hpp>
 
 namespace caspar { namespace diagnostics {
 	
@@ -42,4 +47,59 @@ private:
 safe_ptr<graph> create_graph(const std::string& name);
 safe_ptr<graph> create_graph(const printer& parent_printer);
 	
+//namespace v2
+//{
+//	
+//	struct data
+//	{
+//		float value;
+//	};
+//
+//	class line
+//	{
+//	public:
+//		line();
+//		line(const std::wstring& name);
+//		std::wstring print() const;
+//		void update_value(float value);
+//		void set_value(float value);
+//		void set_tag(const std::wstring& tag);
+//
+//		boost::circular_buffer<data>& ticks();
+//	private:
+//		struct implementation;
+//		std::shared_ptr<implementation> impl_;
+//	};
+//	
+//	class graph;
+//
+//
+//	class graph
+//	{
+//	public:
+//		graph(const std::wstring& name);
+//		graph(const printer& parent_printer);
+//
+//		void update_value(const std::wstring& name, float value);
+//		void set_value(const std::wstring& name, float value);
+//
+//		void set_guide(const std::wstring& name, float value);
+//		void set_color(const std::wstring& name, color c);
+//
+//		color get_color() const;
+//		std::map<std::wstring, line>& get_lines();
+//
+//		std::wstring print() const;
+//
+//		safe_ptr<graph> clone() const;
+//	private:
+//		struct implementation;
+//		std::shared_ptr<implementation> impl_;
+//	};
+//	
+//	static safe_ptr<graph> create_graph(const std::wstring& name);
+//	static safe_ptr<graph> create_graph(const printer& parent_printer);
+//	static std::vector<safe_ptr<graph>> get_all_graphs();
+//}
+
 }}
