f4f409f343f4d5b240a916ce8eea168b7669186b
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-03-05 12:01:57 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@530 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/consumer/decklink/decklink_consumer.cpp
+++ core/consumer/decklink/decklink_consumer.cpp
@@ -359,15 +359,15 @@ std::vector<std::wstring> get_decklink_device_list()
 	try
 	{
 		CComPtr<IDeckLinkIterator> pDecklinkIterator;
-		if(FAILED(pDecklinkIterator.CoCreateInstance(CLSID_CDeckLinkIterator)))
-			return devices;
-		
-		CComPtr<IDeckLink> decklink;
-		for(int n = 0; pDecklinkIterator->Next(&decklink) == S_OK; ++n)	
-		{
-			BSTR model_name = L"Unknown";
-			decklink->GetModelName(&model_name);
-			devices.push_back(L"[" + boost::lexical_cast<std::wstring>(n) + L"] " + model_name);	
+		if(SUCCEEDED(pDecklinkIterator.CoCreateInstance(CLSID_CDeckLinkIterator)))
+		{		
+			CComPtr<IDeckLink> decklink;
+			for(int n = 0; pDecklinkIterator->Next(&decklink) == S_OK; ++n)	
+			{
+				BSTR model_name = L"Unknown";
+				decklink->GetModelName(&model_name);
+				devices.push_back(L"[" + boost::lexical_cast<std::wstring>(n) + L"] " + model_name);	
+			}
 		}
 	}
 	catch(...){}
