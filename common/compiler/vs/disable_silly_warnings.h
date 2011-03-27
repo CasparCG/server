a5d96f340299f70db6f026f700088dd67ee3b517
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-03-27 12:53:06 +0000

    2.0.0.2: Increased warning level.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@580 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- common/compiler/vs/disable_silly_warnings.h
+++ common/compiler/vs/disable_silly_warnings.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #if defined(_MSC_VER)
-#pragma warning (disable : 4482) // nonstandard extension used: enum 'enum' used in qualified name
+#pragma warning (disable : 4100) // unreferenced formal parameter
 #pragma warning (disable : 4127) // conditional expression is constant
-#pragma warning (disable : 4714) // marked as __forceinline not inlined
-#pragma warning (disable : 4503) // decorated name length exceeded, name was truncated
 #pragma warning (disable : 4180) // qualifier applied to function type has no meaning; ignored
+#pragma warning (disable : 4482) // nonstandard extension used: enum 'enum' used in qualified name
+#pragma warning (disable : 4503) // decorated name length exceeded, name was truncated
+#pragma warning (disable : 4512) // assignment operator could not be generated
+#pragma warning (disable : 4714) // marked as __forceinline not inlined
 #endif
 
