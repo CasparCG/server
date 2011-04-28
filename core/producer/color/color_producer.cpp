52b74f3c499e70dc74e9f926b4783968b012cd37
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-04-28 09:11:30 +0000

    2.0.0.2: Added copyright notice to all files.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@649 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/producer/color/color_producer.cpp
+++ core/producer/color/color_producer.cpp
@@ -16,8 +16,7 @@
 *    You should have received a copy of the GNU General Public License
 *    along with CasparCG.  If not, see <http://www.gnu.org/licenses/>.
 *
-*/
- 
+*/ 
 #include "../../stdafx.h"
 
 #include "color_producer.h"
