40bdcefc8b69688e1297b95c184a3f0accb407ca
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-05-07 19:30:26 +0000

    2.0.0.2:  - decklink_consumer: Refactored.  - decklink_consumer: Rewrote and reduced prerolling to decrease latency. Still has a preroll of 4 frames.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@687 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 44238cb1b1143324a30d318ffe060046ba1f3537

--- shell/server.cpp
+++ shell/server.cpp
@@ -120,22 +120,8 @@ struct server::implementation : boost::noncopyable
 					else if(name == "bluefish")					
 						channels_.back()->consumer()->add(index++, bluefish_consumer(xml_consumer.second.get("device", 0), 
 																					xml_consumer.second.get("embedded-audio", true)));					
-					else if(name == "decklink")
-					{
-						decklink_consumer::configuration config;
-						
-						auto key_str = xml_consumer.second.get("key", "default");
-						if(key_str == "internal")
-							config.keyer = decklink_consumer::internal_key;
-						else if(key_str == "external")
-							config.keyer = decklink_consumer::external_key;
-
-						config.device_index = xml_consumer.second.get("device", 0);
-						config.embed_audio = xml_consumer.second.get("embedded-audio", false);
-						config.low_latency = xml_consumer.second.get("low-latency", false);
-
-						channels_.back()->consumer()->add(index++, decklink_consumer(config));
-					}
+					else if(name == "decklink")					
+						channels_.back()->consumer()->add(index++, create_decklink_consumer_ptree(xml_consumer.second));					
 					else if(name == "audio")
 						channels_.back()->consumer()->add(index++, oal_consumer());			
 				}
