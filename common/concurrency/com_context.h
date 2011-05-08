1abcf2e520230c36057c88cb12c087ebe3ea87fe
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-05-08 10:06:23 +0000

    2.0.0.2:  - decklink_consumer: Refactored.  - flash_producer: Refactored. Output empty-frames after error.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@695 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ common/concurrency/com_context.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "executor.h"
+
+#include <Windows.h>
+
+#include <boost/noncopyable.hpp>
+
+#include <functional>
+
+namespace caspar {
+
+template<typename T>
+class com_context : public executor
+{
+	std::unique_ptr<T> instance_;
+public:
+	com_context(const std::wstring& name)
+		: executor(name, true)
+	{
+		executor::begin_invoke([]
+		{
+			::CoInitialize(nullptr);
+		});
+	}
+
+	~com_context()
+	{
+		executor::clear();
+		executor::invoke([&]
+		{
+			instance_.reset(nullptr);
+			::CoUninitialize();
+		});
+	}
+	
+	void reset(const std::function<T*()>& factory)
+	{
+		executor::invoke([&]
+		{
+			if(factory)
+				instance_.reset(factory());
+			else
+				instance_.reset(nullptr);
+		});
+	}
+
+	T& operator*() const { return *instance_.get();}  // noexcept
+
+	T* operator->() const { return instance_.get();}  // noexcept
+
+	T* get() const { return instance_.get();}  // noexcept
+};
+
+}
