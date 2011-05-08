d1e35cf46c485621f85afde729b48079edd46630
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-05-08 13:05:52 +0000

    2.0.0.2:  - diagnostics: Graphs are slightly transparent. git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@697 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- common/diagnostics/graph.cpp
+++ common/diagnostics/graph.cpp
@@ -232,7 +232,7 @@ public:
 			target.Draw(*guide_);
 		
 		glBegin(GL_LINE_STRIP);
-		glColor4f(c_.red, c_.green, c_.blue, 1.0f);		
+		glColor4f(c_.red, c_.green, c_.blue, 0.7f);		
 		for(size_t n = 0; n < line_data_.size(); ++n)		
 			if(line_data_[n].first > -0.5)
 				glVertex3f(x+n*dx, std::max(0.05f, std::min(0.95f, (1.0f-line_data_[n].first)*0.8f + 0.1f)), 0.0f);		
