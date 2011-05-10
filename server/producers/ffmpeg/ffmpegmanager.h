66767a6faa8152e909b9c8e442d3e0b8b50370d4
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-05-10 10:30:56 +0000

    Reverted to 1.7.1 ffmpeg.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/1.8.0.15@713 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- server/producers/ffmpeg/ffmpegmanager.h
+++ server/producers/ffmpeg/ffmpegmanager.h
@@ -1,29 +1,14 @@
-/*
-* copyright (c) 2010 Sveriges Television AB <info@casparcg.com>
-*
-*  This file is part of CasparCG.
-*
-*    CasparCG is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    CasparCG is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-
-*    You should have received a copy of the GNU General Public License
-*    along with CasparCG.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
- 
 #pragma once
 
+//std include
+#include <vector>
+#include <string>
 #include <queue>
 
 #include "..\..\MediaProducer.h"
 #include "..\..\MediaManager.h"
+
+//#include "..\..\utils\thread.h"
 
 namespace caspar {
 namespace ffmpeg {
@@ -32,9 +17,18 @@ class FFMPEGManager : public IMediaManager
 {
 public:
 	FFMPEGManager();
+	virtual ~FFMPEGManager();
 
-	MediaProducerPtr CreateProducer(const tstring& filename);
-	bool getFileInfo(FileInfo* pFileInfo);
+	virtual MediaProducerPtr CreateProducer(const tstring& filename);
+	virtual bool getFileInfo(FileInfo* pFileInfo);
+
+	static const int Alignment;
+	static const int AudioDecompBufferSize;
+private:
+
+	static long static_instanceCount;
+	static bool Initialize();
+	static void Dispose();
 };
 
 }	//namespace ffmpeg
