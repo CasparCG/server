9ad51176ba6d5f69b0cd1d7ae36792957a986e74
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-05-16 16:30:30 +0000

    2.0.0.2: amcp: Changed IS_KEY to KEY_DEPTH which is able to key several layers. Fixed some mayor MIXER command parsing bugs.          mixer: Added support for key_depth.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@760 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 37573b2ec60da488befcfddbfbb06b589e5c2d20

--- core/producer/frame/image_transform.h
+++ core/producer/frame/image_transform.h
@@ -58,8 +58,8 @@ public:
 	image_transform& operator*=(const image_transform &other);
 	const image_transform operator*(const image_transform &other) const;
 
-	void set_is_key(bool value);
-	bool get_is_key() const;
+	void set_key_depth(size_t depth);
+	size_t get_key_depth() const;
 private:
 	double opacity_;
 	double gain_;
@@ -68,7 +68,7 @@ private:
 	std::array<double, 2> key_translation_; 
 	std::array<double, 2> key_scale_; 
 	video_mode::type mode_;
-	bool is_key_;
+	size_t key_depth_;
 };
 
 image_transform tween(double time, const image_transform& source, const image_transform& dest, double duration, const tweener_t& tweener);
