3fb91ff40a0d587e37c57dc2c3a931d9ec7d1f8b
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-06-07 09:43:52 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@850 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/video_channel_context.h
+++ core/video_channel_context.h
@@ -24,39 +24,15 @@ class video_channel_context
 	ogl_device&					ogl_;
 
 public:
-	video_channel_context(int index, ogl_device& ogl, const video_format_desc& format_desc) 
-		: index_(index)
-		, format_desc_(format_desc)
-		, execution_(print() + L"/execution")
-		, destruction_(print() + L"/destruction")
-		, ogl_(ogl)
-	{
-		execution_.set_priority_class(above_normal_priority_class);
-		destruction_.set_priority_class(below_normal_priority_class);
-	}
+	video_channel_context(int index, ogl_device& ogl, const video_format_desc& format_desc);
 
-	const int index() const {return index_;}
-
-	video_format_desc get_format_desc()
-	{
-		tbb::spin_rw_mutex::scoped_lock lock(mutex_, false);
-		return format_desc_;
-	}
-
-	void set_format_desc(const video_format_desc& format_desc)
-	{
-		tbb::spin_rw_mutex::scoped_lock lock(mutex_, true);
-		format_desc_ = format_desc;
-	}
-
-	executor& execution() {return execution_;}
-	executor& destruction() {return destruction_;}
-	ogl_device& ogl() { return ogl_;}
-
-	std::wstring print() const
-	{
-		return L"video_channel[" + boost::lexical_cast<std::wstring>(index_+1) + L"-" +  format_desc_.name + L"]";
-	}
+	const int			index() const;
+	video_format_desc	get_format_desc();
+	void				set_format_desc(const video_format_desc& format_desc);
+	executor&			execution();
+	executor&			destruction();
+	ogl_device&			ogl();
+	std::wstring		print() const;
 };
 	
 }}
