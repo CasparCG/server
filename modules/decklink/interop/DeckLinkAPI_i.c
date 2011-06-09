fe3b7ebbdef12fd1da0c90e1061de8fd3e998337
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-06-09 14:57:47 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@872 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    Former-commit-id: 908dd05301f82efa82b43b7a4d2f455a564a724a

--- modules/decklink/interop/DeckLinkAPI_i.c
+++ modules/decklink/interop/DeckLinkAPI_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Jun 08 10:03:03 2011
+/* at Thu Jun 09 13:48:27 2011
  */
 /* Compiler settings for interop\DeckLinkAPI.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
