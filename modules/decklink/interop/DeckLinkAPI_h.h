063b745f739ec48acf845d935858af6f10fd6ae5
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-06-22 09:30:30 +0000

    2.0. decklink_producer: Reduced frame latency. git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@939 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- modules/decklink/interop/DeckLinkAPI_h.h
+++ modules/decklink/interop/DeckLinkAPI_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Jun 21 10:00:55 2011
+/* at Tue Jun 21 16:03:08 2011
  */
 /* Compiler settings for interop\DeckLinkAPI.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
