0a3e537dada6cd03423da6b892b1461f855b9322
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-01 09:35:16 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1035 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ modules/ffmpeg/producer/filter/scalable_yadif/scalable_yadif.h
@@ -0,0 +1,23 @@
+#pragma once
+
+extern "C" 
+{
+	#define __STDC_CONSTANT_MACROS
+	#define __STDC_LIMIT_MACROS
+	#include <libavutil/cpu.h>
+	#include <libavutil/common.h>
+	#include <libavutil/pixdesc.h>
+	#include <libavfilter/avfilter.h>
+}
+
+int init(AVFilterContext *ctx, const char *args, void *opaque);
+void uninit(AVFilterContext *ctx);
+int query_formats(AVFilterContext *ctx);
+int poll_frame(AVFilterLink *link);
+int request_frame(AVFilterLink *link);
+void start_frame(AVFilterLink *link, AVFilterBufferRef *picref);
+void end_frame(AVFilterLink *link);
+void return_frame(AVFilterContext *ctx, int is_second);
+AVFilterBufferRef *get_video_buffer(AVFilterLink *link, int perms, int w, int h);
+
+void register_scalable_yadif();
