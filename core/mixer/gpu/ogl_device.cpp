ed12e5f6ea5371965d6193b1ed52cc165d80c217
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-08 17:27:53 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1095 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/mixer/gpu/ogl_device.cpp
+++ core/mixer/gpu/ogl_device.cpp
@@ -81,7 +81,11 @@ safe_ptr<device_buffer> ogl_device::create_device_buffer(size_t width, size_t he
 				try
 				{
 					yield();
-					gc().get();
+					BOOST_FOREACH(auto& pools, device_pools_)
+					{
+						BOOST_FOREACH(auto& pool, pools)
+							pool.second->clear();
+					}
 					
 					// Try again
 					buffer.reset(new device_buffer(width, height, stride));
@@ -125,7 +129,11 @@ safe_ptr<host_buffer> ogl_device::create_host_buffer(size_t size, host_buffer::u
 				try
 				{
 					yield();
-					gc().get();
+					BOOST_FOREACH(auto& pools, host_pools_)
+					{
+						BOOST_FOREACH(auto& pool, pools)
+							pool.second->clear();
+					}
 
 					// Try again
 					buffer.reset(new host_buffer(size, usage));
