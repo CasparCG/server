154f5ebb69c09ea7b3eb700b43a2c0a8303be636
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-13 15:19:59 +0000

    2.0. ffmpeg_producer: Improved input buffering params. Muxer will stop requesting when one stream is still not flushed.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1158 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: a387e3f5d730812a2359221d19b1d9fe088a5b93

--- modules/ffmpeg/producer/input.cpp
+++ modules/ffmpeg/producer/input.cpp
@@ -57,8 +57,8 @@ extern "C"
 
 namespace caspar {
 
-static const size_t MAX_BUFFER_COUNT = 64;
-static const size_t MAX_BUFFER_SIZE  = 16 * 1000000;
+static const size_t MAX_BUFFER_COUNT = 32;
+static const size_t MAX_BUFFER_SIZE  = 32 * 1000000;
 	
 struct input::implementation : boost::noncopyable
 {		
