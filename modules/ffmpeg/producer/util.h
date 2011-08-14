6d4d570a4abc7bd36345111b188b5ca1365d3399
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-14 00:28:42 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1161 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- modules/ffmpeg/producer/util.h
+++ modules/ffmpeg/producer/util.h
@@ -28,7 +28,7 @@ static const PixelFormat	CASPAR_PIX_FMT_LUMA = PIX_FMT_MONOBLACK;
 core::video_mode::type		get_mode(AVFrame& frame);
 core::pixel_format::type	get_pixel_format(PixelFormat pix_fmt);
 core::pixel_format_desc		get_pixel_format_desc(PixelFormat pix_fmt, size_t width, size_t height);
-int							make_alpha_format(int format);
+int							make_alpha_format(int format); // NOTE: Be careful about CASPAR_PIX_FMT_LUMA, change it to PIX_FMT_GRAY8 if you want to use the frame inside some ffmpeg function.
 safe_ptr<core::write_frame> make_write_frame(const void* tag, const safe_ptr<AVFrame>& decoded_frame, const safe_ptr<core::frame_factory>& frame_factory, int hints);
 
 }
