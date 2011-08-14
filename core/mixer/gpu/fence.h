b68ec69d5424715d331ee79075f4ca68349caac6
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-14 20:32:06 +0000

    2.0. Added links to ogl specs.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1175 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 11b1245172a1c37df358e6a911b09dc54787fc13

--- core/mixer/gpu/fence.h
+++ core/mixer/gpu/fence.h
@@ -6,6 +6,9 @@ namespace caspar { namespace core {
 	
 class ogl_device;
 
+// Used to avoid blocking ogl thread for async operations. 
+// This is imported when several objects use the same ogl context.
+// Based on http://www.opengl.org/registry/specs/ARB/sync.txt.
 class fence
 {
 public:
