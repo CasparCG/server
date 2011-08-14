bdf1e6ea11cec6ec0d79eda408d9c2c89c82922e
Ronag <Ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-14 00:25:00 +0000

    2.0. Added new pix_fmt, CASPAR_PIX_FMT_LUMA, for using Y/luma channel as alpha.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1160 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 0c70e709c449ebad9ac295aa88332137a810aa7e

--- modules/ffmpeg/producer/util.h
+++ modules/ffmpeg/producer/util.h
@@ -5,6 +5,11 @@
 #include <core/video_format.h>
 #include <core/producer/frame/pixel_format.h>
 
+extern "C"
+{
+	#include <libavutil/pixfmt.h>
+}
+
 struct AVFrame;
 enum PixelFormat;
 
@@ -18,12 +23,12 @@ struct frame_factory;
 
 }
 
+static const PixelFormat	CASPAR_PIX_FMT_LUMA = PIX_FMT_MONOBLACK;
+
 core::video_mode::type		get_mode(AVFrame& frame);
 core::pixel_format::type	get_pixel_format(PixelFormat pix_fmt);
 core::pixel_format_desc		get_pixel_format_desc(PixelFormat pix_fmt, size_t width, size_t height);
-PixelFormat					get_ffmpeg_pixel_format(const core::pixel_format_desc& format_desc);
-safe_ptr<AVFrame>			as_av_frame(const safe_ptr<core::write_frame>& frame);
-bool						try_make_gray(const safe_ptr<AVFrame>& frame);
+int							make_alpha_format(int format);
 safe_ptr<core::write_frame> make_write_frame(const void* tag, const safe_ptr<AVFrame>& decoded_frame, const safe_ptr<core::frame_factory>& frame_factory, int hints);
 
 }
