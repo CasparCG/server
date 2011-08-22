96836dfad2d97e5fe4141311678aa494577770bd
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-08-22 12:05:39 +0000

    2.0. parallel_yadif: Reduced overhead.      ffmpeg: Updated version.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1266 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 6f98c4b8b068f475edca3dc25a10fc35488c5ccf

--- modules/ffmpeg/producer/filter/parallel_yadif.cpp
+++ modules/ffmpeg/producer/filter/parallel_yadif.cpp
@@ -51,8 +51,9 @@ struct parallel_yadif_context
 		int mode;
 	};
 
-	arg	args[64];
+	arg	args[1080];
 	int	index;
+	int last_index;
 
 	parallel_yadif_context() : index(0){}
 };
@@ -64,7 +65,7 @@ void parallel_yadif_filter_line(parallel_yadif_context& ctx, uint8_t *dst, uint8
 	parallel_yadif_context::arg arg = {dst, prev, cur, next, w, prefs, mrefs, parity, mode};
 	ctx.args[ctx.index++] = arg;
 	
-	if(ctx.index == 64)
+	if(ctx.index == ctx.last_index)
 	{		
 		tbb::parallel_for(tbb::blocked_range<size_t>(0, ctx.index), [=](const tbb::blocked_range<size_t>& r)
 		{
@@ -75,13 +76,17 @@ void parallel_yadif_filter_line(parallel_yadif_context& ctx, uint8_t *dst, uint8
 	}
 }
 
+namespace caspar {
+	
+tbb::concurrent_bounded_queue<decltype(org_yadif_filter_line)> parallel_line_func_pool;
+std::array<parallel_yadif_context, 18> ctxs;
+
 #define RENAME(a) f ## a
 
 #define ff(x) \
 void RENAME(x)(uint8_t *dst, uint8_t *prev, uint8_t *cur, uint8_t *next, int w, int prefs, int mrefs, int parity, int mode) \
 {\
-	static parallel_yadif_context ctx;\
-	parallel_yadif_filter_line(ctx, dst, prev, cur, next, w, prefs, mrefs, parity, mode);\
+	parallel_yadif_filter_line(ctxs[x], dst, prev, cur, next, w, prefs, mrefs, parity, mode);\
 }
 
 ff(0); ff(1); ff(2); ff(3); ff(4); ff(5); ff(6); ff(7); ff(8); ff(9); ff(10); ff(11); ff(12); ff(13); ff(14); ff(15); ff(16); ff(17);
@@ -89,9 +94,6 @@ ff(0); ff(1); ff(2); ff(3); ff(4); ff(5); ff(6); ff(7); ff(8); ff(9); ff(10); ff
 void (*fs[])(uint8_t *dst, uint8_t *prev, uint8_t *cur, uint8_t *next, int w, int prefs, int mrefs, int parity, int mode) = 
 {f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17};
 
-namespace caspar {
-	
-tbb::concurrent_bounded_queue<decltype(org_yadif_filter_line)> parallel_line_func_pool;
 
 void init_pool()
 {
@@ -105,11 +107,8 @@ void return_parallel_yadif(void* func)
 		parallel_line_func_pool.push(reinterpret_cast<decltype(fs[0])>(func));
 }
 
-std::shared_ptr<void> make_parallel_yadif(AVFilterContext* ctx, size_t height)
+std::shared_ptr<void> make_parallel_yadif(AVFilterContext* ctx)
 {
-	if(height % 64 != 0)
-		return std::shared_ptr<void>(nullptr, return_parallel_yadif);
-
 	static boost::once_flag flag = BOOST_ONCE_INIT;
 	boost::call_once(&init_pool, flag);
 
@@ -120,8 +119,15 @@ std::shared_ptr<void> make_parallel_yadif(AVFilterContext* ctx, size_t height)
 	if(!parallel_line_func_pool.try_pop(func))	
 		CASPAR_LOG(warning) << "Not enough scalable-yadif context instances. Running non-scalable";
 	else
+	{
+		int index = 0;
+		while(index < sizeof(fs)/sizeof(fs[0]) && fs[index] != func)
+			++index;
+
+		ctxs[index].last_index = ctx->inputs[0]->h;
 		yadif->filter_line = func;
-	
+	}
+
 	return std::shared_ptr<void>(func, return_parallel_yadif);
 }
 
