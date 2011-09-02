283d968622a17a2df63ac7fd89070647f83cd7f4
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-09-02 14:11:57 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1323 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/mixer/audio/audio_util.h
+++ core/mixer/audio/audio_util.h
@@ -13,9 +13,9 @@ static std::vector<int16_t> audio_32_to_16(const boost::iterator_range<int32_t*>
 	std::vector<int16_t> audio16(input.size());
 	auto audio32_ptr = reinterpret_cast<const uint32_t*>(input.begin());
 	auto audio16_ptr = reinterpret_cast<uint32_t*>(audio16.data());
-	auto size		 = input.size();
+	auto size		 = input.size()/2;
 	for(int n = 0; n < size; ++n)		
-		audio16_ptr[n] = (audio32_ptr[n*2+1] & 0xffff0000) | ((audio32_ptr[n*2+0] >> 16) & 0x0000ffff);	
+		audio16_ptr[n] = (audio32_ptr[n*2+1] & 0xffff0000) | (audio32_ptr[n*2+0] >> 16);	
 	return audio16;
 }
 
