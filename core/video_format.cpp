0e9ebb7f74e6c727521901a80086143d93b37c6e
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-09-03 13:00:07 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1330 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/video_format.cpp
+++ core/video_format.cpp
@@ -40,7 +40,7 @@
 	(name),\
 	(48000),\
 	(2),\
-	(static_cast<size_t>(48000.0*2.0/((double)scale/(double)duration)+0.99))\
+	(static_cast<size_t>(48000.0/((double)scale/(double)duration)+0.99)*2)\
 }
 
 namespace caspar { namespace core {
