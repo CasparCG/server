542d4be4feb481fe5b641156c1d8ee198a6c8851
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-09-04 00:09:12 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.0.2@1343 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/mixer/audio/audio_util.h
+++ core/mixer/audio/audio_util.h
@@ -6,16 +6,23 @@
 
 #include <boost/range/iterator_range.hpp>
 
+#include <tbb/cache_aligned_allocator.h>
+
 namespace caspar { namespace core {
 
-static std::vector<int16_t> audio_32_to_16(const boost::iterator_range<int32_t*>& input)
+static std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_32_to_16_sse(const boost::iterator_range<int32_t*>& input)
 {	
-	std::vector<int16_t> audio16(input.size());
-	auto audio32_ptr = reinterpret_cast<const uint32_t*>(input.begin());
-	auto audio16_ptr = reinterpret_cast<uint32_t*>(audio16.data());
-	auto size		 = input.size()/2;
-	for(int n = 0; n < size; ++n)		
-		audio16_ptr[n] = (audio32_ptr[n*2+1] & 0xffff0000) | (audio32_ptr[n*2+0] >> 16);	
+	std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio16(input.size());
+	auto audio32_ptr = reinterpret_cast<const __m128i*>(input.begin());
+	auto audio16_ptr = reinterpret_cast<__m128i*>(audio16.data());
+	auto size		 = input.size();
+	for(int n = 0; n < size/8; ++n)		
+	{
+		auto xmm0 = _mm_srai_epi32(_mm_load_si128(audio32_ptr++), 16);
+		auto xmm1 = _mm_srai_epi32(_mm_load_si128(audio32_ptr++), 16);
+		auto xmm3 = _mm_packs_epi32(xmm0, xmm1);
+		_mm_store_si128(audio16_ptr++, xmm3);
+	}
 	return audio16;
 }
 
