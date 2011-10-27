33dc3f28032456c14cac6469d869bcc332261509
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-10-27 21:41:38 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0 concrt-experimental@1497 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- common/concurrency/governor.h
+++ common/concurrency/governor.h
@@ -11,141 +11,98 @@
 #include <vector>
 
 namespace caspar {
-	
-//namespace Concurrency
-//{	
-//	class semaphore
-//	{
-//	public:
-//		explicit semaphore(LONG capacity)
-//			  : _semaphore_count(capacity)
-//		{
-//		}
-//
-//		// Acquires access to the semaphore.
-//		void acquire()
-//		{
-//			// The capacity of the semaphore is exceeded when the semaphore count 
-//			// falls below zero. When this happens, add the current context to the 
-//			// back of the wait queue and block the current context.
-//			if (InterlockedDecrement(&_semaphore_count) < 0)
-//			{
-//				_waiting_contexts.push(Concurrency::Context::CurrentContext());
-//				Concurrency::Context::Block();
-//			}
-//		}
-//
-//		// Releases access to the semaphore.
-//		void release()
-//		{
-//		  // If the semaphore count is negative, unblock the first waiting context.
-//			if (InterlockedIncrement(&_semaphore_count) <= 0)
-//			{
-//			 // A call to acquire might have decremented the counter, but has not
-//			 // yet finished adding the context to the queue. 
-//			 // Create a spin loop that waits for the context to become available.
-//				Concurrency:: Context* waiting = NULL;
-//				while(!_waiting_contexts.try_pop(waiting))
-//				{
-//					Concurrency::wait(0);
-//				}
-//
-//				// Unblock the context.
-//				 waiting->Unblock();
-//			}
-//		}
-//
-//		void release_all()
-//		{
-//			Concurrency:: Context* waiting = NULL;
-//			while(_waiting_contexts.try_pop(waiting))
-//			{
-//				InterlockedIncrement(&_semaphore_count);
-//				waiting->Unblock();
-//			}
-//		}
-//
-//	private:
-//		// The semaphore count.
-//		LONG _semaphore_count;
-//
-//		// A concurrency-safe queue of contexts that must wait to 
-//		// acquire the semaphore.
-//		Concurrency::concurrent_queue<Concurrency::Context*> _waiting_contexts;
-//	   
-//		semaphore const &operator =(semaphore const&);  // no assignment operator
-//		semaphore(semaphore const &);                   // no copy constructor
-//	};
-//}
-	
+		
 #undef Yield
 
 typedef std::vector<safe_ptr<int>> ticket_t;
 
 class governor : boost::noncopyable
 {
-	tbb::atomic<int> count_;
-	tbb::atomic<int> is_running_;
-	Concurrency::concurrent_queue<Concurrency::Context*> waiting_contexts_;
-
-	void acquire_ticket()
+	struct impl : std::enable_shared_from_this<impl>
 	{
-		if(!is_running_)
-			return;
+		tbb::atomic<int> count_;
+		tbb::atomic<int> is_running_;
+		Concurrency::concurrent_queue<Concurrency::Context*> waiting_contexts_;
 
-		if(count_ < 1)
-			Concurrency::Context::Yield();
-
-		if (--count_ < 0)
+		void acquire_ticket()
 		{
-			auto context = Concurrency::Context::CurrentContext();
-			waiting_contexts_.push(context);
-			context->Block();
-		}
-	}
+			if(!is_running_)
+				return;
 
-	void release_ticket()
-	{
-		if(!is_running_)
-			return;
-
-		if(++count_ <= 0)
-		{
-			Concurrency:: Context* waiting = NULL;
-			while(!waiting_contexts_.try_pop(waiting))
+			if(count_ < 1)
 				Concurrency::Context::Yield();
-			waiting->Unblock();
+
+			if (--count_ < 0)
+			{
+				auto context = Concurrency::Context::CurrentContext();
+				waiting_contexts_.push(context);
+				context->Block();
+			}
 		}
-	}
+
+		void release_ticket()
+		{
+			if(!is_running_)
+				return;
+
+			if(++count_ <= 0)
+			{
+				Concurrency:: Context* waiting = NULL;
+				while(!waiting_contexts_.try_pop(waiting))
+					Concurrency::Context::Yield();
+				waiting->Unblock();
+			}
+		}
+
+	public:
+
+		impl(size_t capacity) 
+		{
+			is_running_ = true;
+			count_ = capacity;
+		}
+	
+		ticket_t acquire()
+		{
+			acquire_ticket();
+		
+			auto self = shared_from_this();
+			ticket_t ticket;
+			ticket.push_back(safe_ptr<int>(new int, [this, self](int* p)
+			{
+				delete p;
+				release_ticket();
+			}));
+			return ticket;
+		}
+
+		void cancel()
+		{
+			is_running_ = false;
+			Concurrency::Context* waiting = NULL;
+			while(waiting_contexts_.try_pop(waiting))
+				waiting->Unblock();
+		}
+	};
 
 public:
-
-	governor(size_t capacity) 
+	governor(size_t capacity) : impl_(new impl(capacity))
 	{
-		is_running_ = true;
-		count_ = capacity;
 	}
 	
 	ticket_t acquire()
 	{
-		acquire_ticket();
-		
-		ticket_t ticket;
-		ticket.push_back(safe_ptr<int>(new int, [this](int* p)
-		{
-			delete p;
-			release_ticket();
-		}));
-		return ticket;
+		return impl_->acquire();
 	}
 
 	void cancel()
 	{
-		is_running_ = false;
-		Concurrency::Context* waiting = NULL;
-		while(waiting_contexts_.try_pop(waiting))
-			waiting->Unblock();
+		impl_->cancel();
 	}
+
+private:
+	safe_ptr<impl> impl_;
+
 };
 
 }
