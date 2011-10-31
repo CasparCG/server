5290c277b7edc628a3b4d51c39c412916fd9c532
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-10-31 19:22:29 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0 concrt-experimental@1528 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    Former-commit-id: bef94a9f20ac1b7f84d9464a8754d0522732fa8d

--- modules/ogl/consumer/ogl_consumer.cpp
+++ modules/ogl/consumer/ogl_consumer.cpp
@@ -444,7 +444,7 @@ public:
 	
 	virtual void initialize(const core::video_format_desc& format_desc)
 	{
-		consumer_.reset();
+		consumer_ = nullptr;
 		consumer_.reset(new ogl_consumer(config_, format_desc));
 	}
 	
