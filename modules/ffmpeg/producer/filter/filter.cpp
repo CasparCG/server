a4921b21c5ec4e475f311130d4375343b081f880
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-11-23 15:08:40 +0000

    ffmpeg update
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.2@1631 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- modules/ffmpeg/producer/filter/filter.cpp
+++ modules/ffmpeg/producer/filter/filter.cpp
@@ -94,10 +94,9 @@ struct filter::implementation
 					args << frame->width << ":" << frame->height << ":" << frame->format << ":" << 0 << ":" << 0 << ":" << 0 << ":" << 0; // don't care about pts and aspect_ratio
 					THROW_ON_ERROR2(avfilter_graph_create_filter(&buffersrc_ctx_, avfilter_get_by_name("buffer"), "src", args.str().c_str(), NULL, graph_.get()), "[filter]");
 
-					// OPIX_FMT_BGRAutput
-					safe_ptr<AVBufferSinkParams> buffersink_params(av_buffersink_params_alloc(), av_free);
-					buffersink_params->pixel_fmts = pix_fmts_.data();
-					THROW_ON_ERROR2(avfilter_graph_create_filter(&buffersink_ctx_, avfilter_get_by_name("buffersink"), "out", NULL, buffersink_params.get(), graph_.get()), "[filter]");
+					//safe_ptr<AVBufferSinkParams> buffersink_params(av_buffersink_params_alloc(), av_free);
+					//buffersink_params->pixel_fmts = pix_fmts_.data();
+					THROW_ON_ERROR2(avfilter_graph_create_filter(&buffersink_ctx_, avfilter_get_by_name("buffersink"), "out", NULL, pix_fmts_.data(), graph_.get()), "[filter]");
 			
 					AVFilterInOut* outputs = avfilter_inout_alloc();
 					AVFilterInOut* inputs  = avfilter_inout_alloc();
