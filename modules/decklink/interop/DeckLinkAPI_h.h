9862ba532c415da4ef8772a4a4ba9ad691612e1c
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-11-23 15:08:40 +0000

    ffmpeg update git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.2@1631 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- modules/decklink/interop/DeckLinkAPI_h.h
+++ modules/decklink/interop/DeckLinkAPI_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Nov 19 12:16:19 2011
+/* at Wed Nov 23 08:55:00 2011
  */
 /* Compiler settings for interop\DeckLinkAPI.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
