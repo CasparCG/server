e646d0c7cf830f2717a2864db42b534870db6f29
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-11-27 12:47:34 +0000

    2.0.2: Updated to boost 1.48.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.2@1678 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/mixer/audio/audio_util.h
+++ core/mixer/audio/audio_util.h
@@ -4,39 +4,50 @@
 
 #include <stdint.h>
 
-#include <boost/range/iterator_range.hpp>
-
 #include <tbb/cache_aligned_allocator.h>
 
 namespace caspar { namespace core {
 
 // NOTE: Input data pointer should be larger than input.size() to allow sse to read beyond
-static std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_32_to_16_sse(const boost::iterator_range<int32_t*>& input)
+template<typename T>
+static std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_32_to_16_sse(const T& audio_data)
 {	
-	std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio16(input.size());
-	auto audio32_ptr = reinterpret_cast<const __m128i*>(input.begin());
-	auto audio16_ptr = reinterpret_cast<__m128i*>(audio16.data());
-	auto size		 = input.size();
+	auto size		 = std::distance(std::begin(audio_data), std::end(audio_data));
+	auto input32	 = &(*std::begin(audio_data));
+	auto output16	 = std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>>(size);
+
+	auto input128	 = reinterpret_cast<const __m128i*>(input32);
+	auto output128	 = reinterpret_cast<__m128i*>(output16.data());
+
 	for(int n = 0; n < size/8; ++n)		
 	{
-		auto xmm0 = _mm_srai_epi32(_mm_load_si128(audio32_ptr++), 16);
-		auto xmm1 = _mm_srai_epi32(_mm_load_si128(audio32_ptr++), 16);
+		auto xmm0 = _mm_srai_epi32(_mm_load_si128(input128++), 16);
+		auto xmm1 = _mm_srai_epi32(_mm_load_si128(input128++), 16);
 		auto xmm3 = _mm_packs_epi32(xmm0, xmm1);
-		_mm_store_si128(audio16_ptr++, xmm3);
+		_mm_store_si128(output128++, xmm3);
 	}
-	return audio16;
+
+	for(int n = size/8; n < size; ++n)
+		output16[n] = input32[n] >> 16;
+
+	return output16;
 }
 
-static std::vector<int8_t> audio_32_to_24(const boost::iterator_range<int32_t*>& input)
+template<typename T>
+static std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>> audio_32_to_24(const T& audio_data)
 {	
-	std::vector<int8_t> audio24(input.size()*3+16);
-	auto audio32_ptr = reinterpret_cast<const uint32_t*>(input.begin());
-	auto audio24_ptr = reinterpret_cast<uint8_t*>(audio24.data());
-	auto size		 = input.size();
-	for(int n = 0; n < size; ++n)		
-		*reinterpret_cast<uint32_t*>(audio24_ptr + n*3) = *(audio32_ptr + n) >> 8;	
-	audio24.resize(input.size()*3);
-	return audio24;
+	auto size		 = std::distance(std::begin(audio_data), std::end(audio_data));
+	auto input8		 = reinterpret_cast<const int8_t*>(&(*std::begin(audio_data)));
+	auto output8	 = std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>>(size*3);
+	
+	for(int n = 0; n < size; ++n)
+	{
+		output8[n*3+0] = input8[n*4+1];
+		output8[n*3+1] = input8[n*4+2];
+		output8[n*3+2] = input8[n*4+3];
+	}
+
+	return output8;
 }
 
 }}
