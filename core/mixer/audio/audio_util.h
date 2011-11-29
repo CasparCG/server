fc99c230dd6ada6ed0fce4879277b70352a20bcb
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-11-29 21:46:36 +0000

    2.0.2: audio_util: Don't do premature-optimization, keep it simple.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.0.2@1721 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/mixer/audio/audio_util.h
+++ core/mixer/audio/audio_util.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 #include <stdint.h>
@@ -7,32 +8,7 @@
 #include <tbb/cache_aligned_allocator.h>
 
 namespace caspar { namespace core {
-
-// NOTE: Input data pointer should be larger than input.size() to allow sse to read beyond
-template<typename T>
-static std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_32_to_16_sse(const T& audio_data)
-{	
-	auto size		 = std::distance(std::begin(audio_data), std::end(audio_data));
-	auto input32	 = &(*std::begin(audio_data));
-	auto output16	 = std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>>(size);
-
-	auto input128	 = reinterpret_cast<const __m128i*>(input32);
-	auto output128	 = reinterpret_cast<__m128i*>(output16.data());
-
-	for(int n = 0; n < size/8; ++n)		
-	{
-		auto xmm0 = _mm_srai_epi32(_mm_load_si128(input128++), 16);
-		auto xmm1 = _mm_srai_epi32(_mm_load_si128(input128++), 16);
-		auto xmm3 = _mm_packs_epi32(xmm0, xmm1);
-		_mm_store_si128(output128++, xmm3);
-	}
-
-	for(int n = size/8; n < size; ++n)
-		output16[n] = input32[n] >> 16;
-
-	return output16;
-}
-
+	
 template<typename T>
 static std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>> audio_32_to_24(const T& audio_data)
 {	
@@ -52,20 +28,17 @@ static std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>> audio_32_to_24(
 }
 
 template<typename T>
-static std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>> audio_32_to_16(const T& audio_data)
+static std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_32_to_16(const T& audio_data)
 {	
 	auto size		 = std::distance(std::begin(audio_data), std::end(audio_data));
-	auto input8		 = reinterpret_cast<const int8_t*>(&(*std::begin(audio_data)));
-	auto output8	 = std::vector<int8_t, tbb::cache_aligned_allocator<int8_t>>();
+	auto input32	 = &(*std::begin(audio_data));
+	auto output16	 = std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>>();
 			
-	output8.reserve(size*2);
+	output16.reserve(size);
 	for(int n = 0; n < size; ++n)
-	{
-		output8.push_back(input8[n*4+2]);
-		output8.push_back(input8[n*4+3]);
-	}
+		output16.push_back((input32[n] >> 16) & 0xFFFF);
 
-	return output8;
+	return output16;
 }
 
 }}
