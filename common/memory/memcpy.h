4b25133091ea3134027200d5c0475e173a6d1c70
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-12-10 21:01:43 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/64bit-exp@1867 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    Former-commit-id: 0bb84e603280080f2e13b2f983d4ee152e1599a9

--- common/memory/memcpy.h
+++ common/memory/memcpy.h
@@ -24,8 +24,6 @@
 #include "../utility/assert.h"
 #include "../memory/safe_ptr.h"
 
-#include <tbb/parallel_for.h>
-
 #include <assert.h>
 #include <intrin.h>
 
@@ -52,25 +50,41 @@ struct memcpy_impl
 
 		for(int n = 0; n < count; n += 256)
 		{
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
+			auto xmm0  = _mm_loadu_si128(source128++);
+			auto xmm1  = _mm_loadu_si128(source128++);
+			auto xmm2  = _mm_loadu_si128(source128++);
+			auto xmm3  = _mm_loadu_si128(source128++);
+			auto xmm4  = _mm_loadu_si128(source128++);
+			auto xmm5  = _mm_loadu_si128(source128++);
+			auto xmm6  = _mm_loadu_si128(source128++);
+			auto xmm7  = _mm_loadu_si128(source128++);
+			auto xmm8  = _mm_loadu_si128(source128++);
+			auto xmm9  = _mm_loadu_si128(source128++);
+			auto xmm10 = _mm_loadu_si128(source128++);
+			auto xmm11 = _mm_loadu_si128(source128++);
+			auto xmm12 = _mm_loadu_si128(source128++);
+			auto xmm13 = _mm_loadu_si128(source128++);
+			auto xmm14 = _mm_loadu_si128(source128++);
+			auto xmm15 = _mm_loadu_si128(source128++);
+			auto xmm16 = _mm_loadu_si128(source128++);
 
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_loadu_si128(source128++));
+			_mm_storeu_si128(dest128++, xmm0);
+			_mm_storeu_si128(dest128++, xmm1);
+			_mm_storeu_si128(dest128++, xmm2);
+			_mm_storeu_si128(dest128++, xmm3);
+			_mm_storeu_si128(dest128++, xmm4);
+			_mm_storeu_si128(dest128++, xmm5);
+			_mm_storeu_si128(dest128++, xmm6);
+			_mm_storeu_si128(dest128++, xmm7);
+			_mm_storeu_si128(dest128++, xmm8);
+			_mm_storeu_si128(dest128++, xmm9);
+			_mm_storeu_si128(dest128++, xmm10);
+			_mm_storeu_si128(dest128++, xmm11);
+			_mm_storeu_si128(dest128++, xmm12);
+			_mm_storeu_si128(dest128++, xmm13);
+			_mm_storeu_si128(dest128++, xmm14);
+			_mm_storeu_si128(dest128++, xmm15);
+			_mm_storeu_si128(dest128++, xmm16);
 		}
 		
 		return memcpy(reinterpret_cast<int8_t*>(dest)+count, reinterpret_cast<const int8_t*>(source)+count, rest);
@@ -90,30 +104,46 @@ struct memcpy_impl<true_type, true_type>
 		auto dest128   = reinterpret_cast<__m128i*>(dest);
 		auto source128 = reinterpret_cast<const __m128i*>(source);
 		
-		size_t rest = count & 255;
-		count &= ~255;
+		size_t rest = count % 256;
+		count -= rest;
 
 		for(int n = 0; n < count; n += 256)
 		{
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
+			auto xmm0  = _mm_load_si128(source128++);
+			auto xmm1  = _mm_load_si128(source128++);
+			auto xmm2  = _mm_load_si128(source128++);
+			auto xmm3  = _mm_load_si128(source128++);
+			auto xmm4  = _mm_load_si128(source128++);
+			auto xmm5  = _mm_load_si128(source128++);
+			auto xmm6  = _mm_load_si128(source128++);
+			auto xmm7  = _mm_load_si128(source128++);
+			auto xmm8  = _mm_load_si128(source128++);
+			auto xmm9  = _mm_load_si128(source128++);
+			auto xmm10 = _mm_load_si128(source128++);
+			auto xmm11 = _mm_load_si128(source128++);
+			auto xmm12 = _mm_load_si128(source128++);
+			auto xmm13 = _mm_load_si128(source128++);
+			auto xmm14 = _mm_load_si128(source128++);
+			auto xmm15 = _mm_load_si128(source128++);
+			auto xmm16 = _mm_load_si128(source128++);
 
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_load_si128(source128++));
+			_mm_stream_si128(dest128++, xmm0);
+			_mm_stream_si128(dest128++, xmm1);
+			_mm_stream_si128(dest128++, xmm2);
+			_mm_stream_si128(dest128++, xmm3);
+			_mm_stream_si128(dest128++, xmm4);
+			_mm_stream_si128(dest128++, xmm5);
+			_mm_stream_si128(dest128++, xmm6);
+			_mm_stream_si128(dest128++, xmm7);
+			_mm_stream_si128(dest128++, xmm8);
+			_mm_stream_si128(dest128++, xmm9);
+			_mm_stream_si128(dest128++, xmm10);
+			_mm_stream_si128(dest128++, xmm11);
+			_mm_stream_si128(dest128++, xmm12);
+			_mm_stream_si128(dest128++, xmm13);
+			_mm_stream_si128(dest128++, xmm14);
+			_mm_stream_si128(dest128++, xmm15);
+			_mm_stream_si128(dest128++, xmm16);
 		}
 		
 		return memcpy(reinterpret_cast<int8_t*>(dest)+count, reinterpret_cast<const int8_t*>(source)+count, rest);
@@ -132,30 +162,46 @@ struct memcpy_impl<true_type, false_type>
 		auto dest128   = reinterpret_cast<__m128i*>(dest);
 		auto source128 = reinterpret_cast<const __m128i*>(source);
 		
-		size_t rest = count & 255;
-		count &= ~255;
+		size_t rest = count % 256;
+		count -= rest;
 
 		for(int n = 0; n < count; n += 256)
 		{
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
+			auto xmm0  = _mm_loadu_si128(source128++);
+			auto xmm1  = _mm_loadu_si128(source128++);
+			auto xmm2  = _mm_loadu_si128(source128++);
+			auto xmm3  = _mm_loadu_si128(source128++);
+			auto xmm4  = _mm_loadu_si128(source128++);
+			auto xmm5  = _mm_loadu_si128(source128++);
+			auto xmm6  = _mm_loadu_si128(source128++);
+			auto xmm7  = _mm_loadu_si128(source128++);
+			auto xmm8  = _mm_loadu_si128(source128++);
+			auto xmm9  = _mm_loadu_si128(source128++);
+			auto xmm10 = _mm_loadu_si128(source128++);
+			auto xmm11 = _mm_loadu_si128(source128++);
+			auto xmm12 = _mm_loadu_si128(source128++);
+			auto xmm13 = _mm_loadu_si128(source128++);
+			auto xmm14 = _mm_loadu_si128(source128++);
+			auto xmm15 = _mm_loadu_si128(source128++);
+			auto xmm16 = _mm_loadu_si128(source128++);
 
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
-			_mm_stream_si128(dest128++, _mm_loadu_si128(source128++));
+			_mm_stream_si128(dest128++, xmm0);
+			_mm_stream_si128(dest128++, xmm1);
+			_mm_stream_si128(dest128++, xmm2);
+			_mm_stream_si128(dest128++, xmm3);
+			_mm_stream_si128(dest128++, xmm4);
+			_mm_stream_si128(dest128++, xmm5);
+			_mm_stream_si128(dest128++, xmm6);
+			_mm_stream_si128(dest128++, xmm7);
+			_mm_stream_si128(dest128++, xmm8);
+			_mm_stream_si128(dest128++, xmm9);
+			_mm_stream_si128(dest128++, xmm10);
+			_mm_stream_si128(dest128++, xmm11);
+			_mm_stream_si128(dest128++, xmm12);
+			_mm_stream_si128(dest128++, xmm13);
+			_mm_stream_si128(dest128++, xmm14);
+			_mm_stream_si128(dest128++, xmm15);
+			_mm_stream_si128(dest128++, xmm16);
 		}
 		
 		return memcpy(reinterpret_cast<int8_t*>(dest)+count, reinterpret_cast<const int8_t*>(source)+count, rest);
@@ -174,69 +220,69 @@ struct memcpy_impl<false_type, true_type>
 		auto dest128   = reinterpret_cast<__m128i*>(dest);
 		auto source128 = reinterpret_cast<const __m128i*>(source);
 		
-		size_t rest = count & 255;
-		count &= ~255;
+		size_t rest = count % 256;
+		count -= rest;
 
 		for(int n = 0; n < count; n += 256)
 		{
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
+			auto xmm0  = _mm_load_si128(source128++);
+			auto xmm1  = _mm_load_si128(source128++);
+			auto xmm2  = _mm_load_si128(source128++);
+			auto xmm3  = _mm_load_si128(source128++);
+			auto xmm4  = _mm_load_si128(source128++);
+			auto xmm5  = _mm_load_si128(source128++);
+			auto xmm6  = _mm_load_si128(source128++);
+			auto xmm7  = _mm_load_si128(source128++);
+			auto xmm8  = _mm_load_si128(source128++);
+			auto xmm9  = _mm_load_si128(source128++);
+			auto xmm10 = _mm_load_si128(source128++);
+			auto xmm11 = _mm_load_si128(source128++);
+			auto xmm12 = _mm_load_si128(source128++);
+			auto xmm13 = _mm_load_si128(source128++);
+			auto xmm14 = _mm_load_si128(source128++);
+			auto xmm15 = _mm_load_si128(source128++);
+			auto xmm16 = _mm_load_si128(source128++);
 
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
-			_mm_storeu_si128(dest128++, _mm_load_si128(source128++));
+			_mm_storeu_si128(dest128++, xmm0);
+			_mm_storeu_si128(dest128++, xmm1);
+			_mm_storeu_si128(dest128++, xmm2);
+			_mm_storeu_si128(dest128++, xmm3);
+			_mm_storeu_si128(dest128++, xmm4);
+			_mm_storeu_si128(dest128++, xmm5);
+			_mm_storeu_si128(dest128++, xmm6);
+			_mm_storeu_si128(dest128++, xmm7);
+			_mm_storeu_si128(dest128++, xmm8);
+			_mm_storeu_si128(dest128++, xmm9);
+			_mm_storeu_si128(dest128++, xmm10);
+			_mm_storeu_si128(dest128++, xmm11);
+			_mm_storeu_si128(dest128++, xmm12);
+			_mm_storeu_si128(dest128++, xmm13);
+			_mm_storeu_si128(dest128++, xmm14);
+			_mm_storeu_si128(dest128++, xmm15);
+			_mm_storeu_si128(dest128++, xmm16);
 		}
 
 		return memcpy(reinterpret_cast<int8_t*>(dest)+count, reinterpret_cast<const int8_t*>(source)+count, rest);
 	}
 };
 
-template<typename T>
-static void* fast_memcpy(void* dest, const void* source, size_t count)
-{   
-	auto dest8			= reinterpret_cast<char*>(dest);
-	auto source8		= reinterpret_cast<const char*>(source);
-		
-	size_t rest = count & 2047;
-	count &= ~2047;
-		
-	tbb::affinity_partitioner ap;
-	tbb::parallel_for(tbb::blocked_range<size_t>(0, count/256), [&](const tbb::blocked_range<size_t>& r)
-	{       
-		T()(reinterpret_cast<char*>(dest) + r.begin()*256, reinterpret_cast<const char*>(source) + r.begin()*256, r.size()*256);   
-	}, ap);
-	
-	return T()(dest8+count, source8+count, rest);
-}
 }
 
 template<typename T>
 T* fast_memcpy(T* dest, const void* source, size_t count)
 {  
-	auto s_align = reinterpret_cast<int>(source) & 15;
-	auto d_align = reinterpret_cast<int>(dest) & 15;
+	//auto s_align = reinterpret_cast<int>(source) & 15;
+	//auto d_align = reinterpret_cast<int>(dest) & 15;
 	
-	if(s_align == 0 && s_align == 0)
-		return reinterpret_cast<T*>(detail::fast_memcpy<detail::memcpy_impl<detail::true_type, detail::true_type>>(dest, source, count));
-	else if(d_align == 0)
-		return reinterpret_cast<T*>(detail::fast_memcpy<detail::memcpy_impl<detail::true_type, detail::false_type>>(dest, source, count));
-	else if(s_align == 0)
-		return reinterpret_cast<T*>(detail::fast_memcpy<detail::memcpy_impl<detail::false_type, detail::true_type>>(dest, source, count));
+	//if(s_align == 0 && s_align == 0)
+	//	return reinterpret_cast<T*>(detail::memcpy_impl<detail::true_type, detail::true_type>()(dest, source, count));
+	//else if(d_align == 0)
+	//	return reinterpret_cast<T*>(detail::memcpy_impl<detail::true_type, detail::false_type>()(dest, source, count));
+	//else if(s_align == 0)
+	//	return reinterpret_cast<T*>(detail::memcpy_impl<detail::false_type, detail::true_type>()(dest, source, count));
 
-	return reinterpret_cast<T*>(detail::fast_memcpy<detail::memcpy_impl<detail::false_type, detail::false_type>>(dest, source, count));
+	//return reinterpret_cast<T*>(detail::memcpy_impl<detail::false_type, detail::false_type>()(dest, source, count));
+	return reinterpret_cast<T*>(memcpy(dest, source, count));
 }
 
 }
