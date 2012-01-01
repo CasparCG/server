ceea92f178e493ee907d85bf40b0e2ee5518d452
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-01-01 21:41:40 +0000

    2.1.0: Fixed compilation.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2015 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/producer/layer.cpp
+++ core/producer/layer.cpp
@@ -151,7 +151,7 @@ public:
 		auto nb_frames = foreground_->nb_frames();
 
 		info.add(L"nb_frames",	 nb_frames == std::numeric_limits<int64_t>::max() ? -1 : nb_frames);
-		info.add(L"frames-left", nb_frames == std::numeric_limits<int64_t>::max() ? -1 : (foreground_->nb_frames() - frame_number_ - auto_play_delta_));
+		info.add(L"frames-left", nb_frames == std::numeric_limits<int64_t>::max() ? -1 : (foreground_->nb_frames() - frame_number_ - (auto_play_delta_ ? *auto_play_delta_ : 0)));
 		info.add_child(L"foreground.producer", foreground_->info());
 		info.add_child(L"background.producer", background_->info());
 		return info;
