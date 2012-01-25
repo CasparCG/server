6eb31b2a10daf98bfb805746ffd71299a157d613
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-01-25 21:44:11 +0000

    2.1.0: Merged trunk: r2166 Just ignore WM_TIMER messages we are sure should be ignored. --------------------- r2165 ffmpeg_consumer: Fixed options parsing. --------------------- r2164 Cosmetics. --------------------- r2163 ffmpeg_consumer: Fixed options and changed syntex to more closely resemble ffmpeg. --------------------- r2162 flash_producer: Added comment. --------------------- r2161 --------------------- r2160 flash_producer: Dispatch windows messages. --------------------- r2159 --------------------- r2154 --------------------- git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2167 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ core/mixer/gpu/device_buffer.h
@@ -0,0 +1,60 @@
+/*
+* Copyright (c) 2011 Sveriges Television AB <info@casparcg.com>
+*
+* This file is part of CasparCG (www.casparcg.com).
+*
+* CasparCG is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* CasparCG is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CasparCG. If not, see <http://www.gnu.org/licenses/>.
+*
+* Author: Robert Nagy, ronag89@gmail.com
+*/
+
+#pragma once
+
+#include <common/memory/safe_ptr.h>
+#include <common/forward.h>
+
+#include <boost/noncopyable.hpp>
+
+FORWARD1(boost, template<typename> class unique_future);
+
+namespace caspar { namespace core { namespace gpu {
+		
+class host_buffer;
+class accelerator;
+
+class device_buffer : boost::noncopyable
+{
+public:	
+	int stride() const;	
+	int width() const;
+	int height() const;
+		
+	void copy_from(const safe_ptr<host_buffer>& source);
+	void copy_to(const safe_ptr<host_buffer>& dest);
+private:
+	friend class accelerator;
+	friend class image_kernel;
+	device_buffer(std::weak_ptr<accelerator> parent, int width, int height, int stride);
+			
+	void bind(int index);
+	void unbind();
+	int id() const;
+
+	struct impl;
+	safe_ptr<impl> impl_;
+};
+	
+unsigned int format(int stride);
+
+}}}
