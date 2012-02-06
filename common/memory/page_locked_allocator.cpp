df5351b5a58fe0b5a94e51d96d4d307f1b6391b1
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-02-06 20:11:37 +0000

    2.1.0: page_locked_allocator: Refactored. git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2280 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ common/memory/page_locked_allocator.cpp
@@ -0,0 +1,66 @@
+#include "../stdafx.h"
+
+#include "../os/windows/windows.h"
+
+#include <unordered_map>
+#include <tbb/mutex.h>
+#include <tbb/atomic.h>
+
+namespace caspar { namespace detail {
+	
+tbb::mutex							g_mutex;
+std::unordered_map<void*, size_t>	g_map;
+size_t								g_free;
+
+void allocate_store(size_t size)
+{		
+	SIZE_T workingSetMinSize = 0, workingSetMaxSize = 0;
+	if(::GetProcessWorkingSetSize(::GetCurrentProcess(), &workingSetMinSize, &workingSetMaxSize))
+	{			
+		workingSetMinSize += size;
+		workingSetMaxSize += size;
+
+		if(!::SetProcessWorkingSetSize(::GetCurrentProcess(), workingSetMinSize, workingSetMaxSize))		
+			throw std::bad_alloc();		
+
+		g_free += size;
+	}
+}
+
+void* alloc_page_locked(size_t size)
+{
+	tbb::mutex::scoped_lock lock(g_mutex);
+
+	if(g_free < size)
+		allocate_store(size);
+
+	auto p = ::VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if(!p)
+		throw std::bad_alloc();
+
+	if(::VirtualLock(p, size) == 0)	
+	{
+		::VirtualFree(p, 0, MEM_RELEASE);
+		throw std::bad_alloc();
+	}
+		
+	g_free   -= size;
+	g_map[p]	= size;
+	return p;
+
+}
+
+void free_page_locked(void* p)
+{		
+	tbb::mutex::scoped_lock lock(g_mutex);
+
+	if(g_map.find(p) == g_map.end())
+		return;
+
+	::VirtualFree(p, 0, MEM_RELEASE);
+
+	g_free += g_map[p];
+	g_map.erase(p);
+}
+
+}}
