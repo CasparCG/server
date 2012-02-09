7ce2eefd5b1a79e46c3ed53f3db67adbb2a31639
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-02-09 18:36:09 +0000

    2.1.0: -cpu/image_mixer: Added support for interlacing.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2320 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- accelerator/cpu/util/blend.h
+++ accelerator/cpu/util/blend.h
@@ -8,8 +8,6 @@
 
 #include "simd.h"
 
-#include <tbb/parallel_for.h>
-
 #include <intrin.h>
 #include <stdint.h>
 
@@ -35,31 +33,28 @@ static void blend(uint8_t* dest, const uint8_t* source1, const uint8_t* source2,
     const xmm_epi16 round  = 128;
     const xmm_epi16 lomask = 0x00FF;
 		
-	tbb::parallel_for(tbb::blocked_range<size_t>(0, count/sizeof(xmm_epi8)), [&](const tbb::blocked_range<size_t>& r)
+	for(auto n = 0; n < count; n += 16)    
 	{
-		for(auto n = r.begin(); n != r.end(); ++n)    
-		{
-			auto s = xmm_epi16::load(source1+n*16);
-			auto d = xmm_epi16::load(source2+n*16);
+		auto s = xmm_epi16::load(source1+n);
+		auto d = xmm_epi16::load(source2+n);
 
-			// T(S, D) = S * D[A] + 0x80
-			auto xxxa	= xmm_cast<xmm_epi32>(d) >> 24;
-			auto xaxa	= xmm_cast<xmm_epi16>((xxxa << 16) | xxxa);
+		// T(S, D) = S * D[A] + 0x80
+		auto xxxa	= xmm_cast<xmm_epi32>(d) >> 24;
+		auto xaxa	= xmm_cast<xmm_epi16>((xxxa << 16) | xxxa);
 			      
-			auto xbxr	= s & lomask;
-			auto t1		= xmm_epi16::multiply_low(xbxr, xaxa) + round;    
+		auto xbxr	= s & lomask;
+		auto t1		= xmm_epi16::multiply_low(xbxr, xaxa) + round;    
 			
-			auto xaxg	= s >> 8;
-			auto t2		= xmm_epi16::multiply_low(xaxg, xaxa) + round;
+		auto xaxg	= s >> 8;
+		auto t2		= xmm_epi16::multiply_low(xaxg, xaxa) + round;
 		
-			// C(S, D) = S + D - (((T >> 8) + T) >> 8);
-			auto bxrx	= (t1 >> 8) + t1;      
-			auto axgx	= (t2 >> 8) + t2;    
-			auto bgra	= xmm_cast<xmm_epi8>((bxrx >> 8) | xmm_epi16::and_not(axgx, lomask));
+		// C(S, D) = S + D - (((T >> 8) + T) >> 8);
+		auto bxrx	= (t1 >> 8) + t1;      
+		auto axgx	= (t2 >> 8) + t2;    
+		auto bgra	= xmm_cast<xmm_epi8>((bxrx >> 8) | xmm_epi16::and_not(axgx, lomask));
 				
-			xmm_epi8::stream(xmm_cast<xmm_epi8>(s) + (xmm_cast<xmm_epi8>(d) - bgra), dest + n*16);
-		}     
-	});
+		xmm_epi8::stream(xmm_cast<xmm_epi8>(s) + (xmm_cast<xmm_epi8>(d) - bgra), dest+n);
+	} 
 }
 
 }}}
