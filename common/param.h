41083582cc67d5c76f2fff5890836776190e05b4
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-02-13 09:17:08 +0000

    2.1.0: Updated param parsing.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2385 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 1f82a26058f8931d7a84699ffad997df62166269

--- common/param.h
+++ common/param.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "except.h"
+
 #include <boost/lexical_cast.hpp>
 
 #include <type_traits>
@@ -11,26 +13,40 @@ template<typename T, typename C>
 typename std::enable_if<!std::is_convertible<T, std::wstring>::value, typename std::decay<T>::type>::type get_param(const std::wstring& name, C&& params, T fail_value = T())
 {	
 	auto it = std::find(std::begin(params), std::end(params), name);
-	if(it == params.end() || ++it == params.end())	
+	if(it == params.end())	
 		return fail_value;
 	
-	T value = fail_value;
 	try
 	{
-		value = boost::lexical_cast<std::decay<T>::type>(*it);
-	}
-	catch(boost::bad_lexical_cast&){}
+		if(++it == params.end())
+			throw std::out_of_range("");
 
-	return value;
+		return boost::lexical_cast<std::decay<T>::type>(*it);
+	}
+	catch(...)
+	{		
+		BOOST_THROW_EXCEPTION(invalid_argument() << msg_info("Failed to parse param.") << arg_name_info(name) << nested_exception(std::current_exception()));
+	}
 }
 
 template<typename C>
 std::wstring get_param(const std::wstring& name, C&& params, const std::wstring& fail_value = L"")
 {	
 	auto it = std::find(std::begin(params), std::end(params), name);
-	if(it == params.end() || ++it == params.end())	
+	if(it == params.end())	
 		return fail_value;
-	return *it;
+	
+	try
+	{
+		if(++it == params.end())
+			throw std::out_of_range("");
+
+		return *it;	
+	}
+	catch(...)
+	{		
+		BOOST_THROW_EXCEPTION(invalid_argument() << msg_info("Failed to parse param.") << arg_name_info(name) << nested_exception(std::current_exception()));
+	}
 }
 
 }
