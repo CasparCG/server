cd414d1606a7164d179a09839a6143c24164989c
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-03-18 13:00:36 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2657 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/producer/frame_producer.cpp
+++ core/producer/frame_producer.cpp
@@ -49,7 +49,6 @@ void register_producer_factory(const producer_factory_t& factory)
 struct frame_producer_base::impl
 {
 	tbb::atomic<uint32_t>	frame_number_;
-	tbb::atomic<bool>		paused_;
 	frame_producer_base&	self_;
 	draw_frame				last_frame_;
 
@@ -58,14 +57,10 @@ struct frame_producer_base::impl
 		, last_frame_(draw_frame::empty())
 	{
 		frame_number_ = 0;
-		paused_ = false;
 	}
 	
 	draw_frame receive()
 	{
-		if(paused_)
-			return self_.last_frame();
-
 		auto frame = draw_frame::push(self_.receive_impl());
 		if(frame == draw_frame::late())
 			return frame;
@@ -74,12 +69,7 @@ struct frame_producer_base::impl
 
 		return last_frame_ = frame;
 	}
-
-	void paused(bool value)
-	{
-		paused_ = value;
-	}
-
+	
 	draw_frame last_frame() const
 	{
 		return draw_frame::still(last_frame_);
@@ -101,7 +91,7 @@ draw_frame frame_producer_base::receive()
 
 void frame_producer_base::paused(bool value)
 {
-	impl_->paused(value);
+	BOOST_THROW_EXCEPTION(not_supported());
 }
 
 draw_frame frame_producer_base::last_frame() const
