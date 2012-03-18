0cc4545a249888e9c242ba11ef89b843da1903ca
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-03-18 22:59:24 +0000

    2.1.0: -oal_consumer: Rewritten to push buffers instead of pull.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2675 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: 8c9370c7bfd8f78b0b020ca48bf7ecb03468cec9

--- modules/oal/consumer/oal_consumer.cpp
+++ modules/oal/consumer/oal_consumer.cpp
@@ -22,6 +22,7 @@
 #include "oal_consumer.h"
 
 #include <common/except.h>
+#include <common/executor.h>
 #include <common/diagnostics/graph.h>
 #include <common/log.h>
 #include <common/utf.h>
@@ -33,53 +34,93 @@
 #include <core/mixer/audio/audio_mixer.h>
 #include <core/video_format.h>
 
-#include <SFML/Audio/SoundStream.hpp>
-
 #include <boost/circular_buffer.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/timer.hpp>
+#include <boost/foreach.hpp>
 
 #include <tbb/concurrent_queue.h>
+
+#include <al/alc.h>
+#include <al/al.h>
+
+#include <array>
 
 namespace caspar { namespace oal {
 
 typedef std::vector<int16_t, tbb::cache_aligned_allocator<int16_t>> audio_buffer_16;
 
-struct oal_consumer : public core::frame_consumer,  public sf::SoundStream
+struct oal_consumer : public core::frame_consumer
 {
 	spl::shared_ptr<diagnostics::graph>					graph_;
 	boost::timer										perf_timer_;
 	int													channel_index_;
-
-	tbb::concurrent_bounded_queue<std::shared_ptr<audio_buffer_16>>	input_;
-	boost::circular_buffer<audio_buffer_16>				container_;
-	tbb::atomic<bool>									is_running_;
-	bool												set_thread_priority_;
-
+	
 	core::video_format_desc								format_desc_;
+
+	ALCdevice*											device_;
+	ALCcontext*											context_;
+	ALuint												source_;
+	std::array<ALuint, 3>								buffers_;
+
+	executor											executor_;
+
 public:
 	oal_consumer() 
-		: container_(16)
-		, channel_index_(-1)
-		, set_thread_priority_(true)
+		: channel_index_(-1)
+		, device_(0)
+		, context_(0)
+		, source_(0)
+		, executor_(L"oal_consumer")
 	{
+		buffers_.assign(0);
+
 		graph_->set_color("tick-time", diagnostics::color(0.0f, 0.6f, 0.9f));	
 		graph_->set_color("dropped-frame", diagnostics::color(0.3f, 0.6f, 0.3f));
 		diagnostics::register_graph(graph_);
+		
+		executor_.invoke([=]
+		{		
+			device_ = alcOpenDevice(nullptr);
 
-		is_running_ = true;
-		input_.set_capacity(1);
+			if(!device_)
+				BOOST_THROW_EXCEPTION(invalid_operation());
+
+			context_ = alcCreateContext(device_, nullptr);
+
+			if(!context_)
+				BOOST_THROW_EXCEPTION(invalid_operation());
+			
+			if(alcMakeContextCurrent(context_) == ALC_FALSE)
+				BOOST_THROW_EXCEPTION(invalid_operation());
+		});
 	}
 
 	~oal_consumer()
 	{
-		is_running_ = false;
-		input_.try_push(std::make_shared<audio_buffer_16>());
-		input_.try_push(std::make_shared<audio_buffer_16>());
-		Stop();
-		input_.try_push(std::make_shared<audio_buffer_16>());
-		input_.try_push(std::make_shared<audio_buffer_16>());
+		executor_.begin_invoke([=]
+		{		
+			if(source_)
+			{
+				alSourceStop(source_);
+				alDeleteSources(1, &source_);
+			}
+
+			BOOST_FOREACH(auto& buffer, buffers_)
+			{
+				if(buffer)
+					alDeleteBuffers(1, &buffer);
+			};
+
+			alcMakeContextCurrent(nullptr);
+
+			if(context_)
+				alcDestroyContext(context_);
+
+			if(device_)
+				alcCloseDevice(device_);
+		});
 	}
 
 	// frame consumer
@@ -89,18 +130,47 @@ public:
 		format_desc_	= format_desc;		
 		channel_index_	= channel_index;
 		graph_->set_text(print());
+		
+		executor_.begin_invoke([=]
+		{		
+			alGenBuffers(static_cast<ALsizei>(buffers_.size()), buffers_.data());
+			alGenSources(1, &source_);
 
-		if(Status() != Playing)
-		{
-			sf::SoundStream::Initialize(2, 48000);
-			Play();		
-		}
+			for(std::size_t n = 0; n < buffers_.size(); ++n)
+			{
+				std::vector<int16_t> audio(format_desc_.audio_cadence[n % format_desc_.audio_cadence.size()], 0);
+				alBufferData(buffers_[n], AL_FORMAT_STEREO16, audio.data(), static_cast<ALsizei>(audio.size()*sizeof(int16_t)), 48000);
+				alSourceQueueBuffers(source_, 1, &buffers_[n]);
+			}
+			
+			alSourcei(source_, AL_LOOPING, AL_FALSE);
+		});
 	}
 	
 	bool send(core::const_frame frame) override
 	{			
-		if(!input_.try_push(std::make_shared<audio_buffer_16>(core::audio_32_to_16(frame.audio_data()))))
-			graph_->set_tag("dropped-frame");
+		executor_.begin_invoke([=]
+		{
+			ALenum state; 
+			alGetSourcei(source_, AL_SOURCE_STATE,&state);
+			if(state != AL_PLAYING)
+				alSourcePlay(source_);			
+
+			auto audio = core::audio_32_to_16(frame.audio_data());
+			
+			ALuint buffer = 0;  
+			alSourceUnqueueBuffers(source_, 1, &buffer);
+			if(buffer)
+			{
+				alBufferData(buffer, AL_FORMAT_STEREO16, audio.data(), static_cast<ALsizei>(audio.size()*sizeof(int16_t)), 48000);
+				alSourceQueueBuffers(source_, 1, &buffer);
+			}
+			else
+				graph_->set_tag("dropped-frame");
+
+			graph_->set_value("tick-time", perf_timer_.elapsed()*format_desc_.fps*0.5);		
+			perf_timer_.restart();
+		});
 
 		return true;
 	}
@@ -131,30 +201,7 @@ public:
 	{
 		return 3;
 	}
-
-	// oal_consumer
-	
-	bool OnGetData(sf::SoundStream::Chunk& data) override
-	{		
-		if(set_thread_priority_)
-		{
-			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
-			set_thread_priority_ = false;
-		}
-
-		std::shared_ptr<audio_buffer_16> audio_data;		
-		input_.pop(audio_data);
-				
-		container_.push_back(std::move(*audio_data));
-		data.Samples = container_.back().data();
-		data.NbSamples = container_.back().size();	
 		
-		graph_->set_value("tick-time", perf_timer_.elapsed()*format_desc_.fps*0.5);		
-		perf_timer_.restart();
-
-		return is_running_;
-	}
-
 	int index() const override
 	{
 		return 500;
