4bcc695a3acc4395f84fcacd69364dd420a539ad
ronag <ronag@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-03-27 18:08:38 +0000

    2.1.0: Extracted "port" class. git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@2768 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ core/consumer/port.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "../monitor/monitor.h"
+
+#include <common/memory.h>
+
+#include <boost/property_tree/ptree_fwd.hpp>
+
+namespace caspar { namespace core {
+
+class port : public monitor::observable
+{
+	port(const port&);
+	port& operator=(const port&);
+public:
+
+	// Static Members
+
+	// Constructors
+
+	port(int index, int channel_index, spl::shared_ptr<class frame_consumer> consumer);
+	port(port&& other);
+	~port();
+
+	// Member Functions
+
+	port& operator=(port&& other);
+
+	bool send(class const_frame frame);	
+
+	// monitor::observable
+	
+	void subscribe(const monitor::observable::observer_ptr& o) override;
+	void unsubscribe(const monitor::observable::observer_ptr& o) override;
+
+	// Properties
+
+	void video_format_desc(const struct video_format_desc& format_desc);
+	int buffer_depth() const;
+	bool has_synchronization_clock() const;
+	boost::property_tree::wptree info() const;
+private:
+	struct impl;
+	std::unique_ptr<impl> impl_;
+};
+
+}}
