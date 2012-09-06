8e8147f03959f71cb9ffc1b2c1383a3f90a9b3c6
hellgore <hellgore@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-09-06 14:43:49 +0000

    Refactored executor to support try_begin_invoke which does not block until room is available in the executor queue, but returns immediately instead.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/branches/2.1.0@3248 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d
    
    Former-commit-id: f1169352e92ac3537803b172bf59d4d137113cab

---
+++ common/semaphore.h
@@ -0,0 +1,158 @@
+/*
+* Copyright (c) 2011 Sveriges Television AB <info@casparcg.com>
+*
+* This file is part of CasparCG (www.casparcg.com).
+*
+* CasparCG is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* CasparCG is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CasparCG. If not, see <http://www.gnu.org/licenses/>.
+*
+* Author: Helge Norberg, helge.norberg@svt.se
+*/
+
+#pragma once
+
+#include <cmath>
+
+#include <boost/noncopyable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+
+namespace caspar {
+
+template <class N, class Func>
+void repeat_n(N times_to_repeat_block, const Func& func)
+{
+	for (N i = 0; i < times_to_repeat_block; ++i)
+	{
+		func();
+	}
+}
+
+/**
+ * Counting semaphore modelled after java.util.concurrent.Semaphore
+ */
+class semaphore : boost::noncopyable
+{
+	mutable boost::mutex mutex_;
+	unsigned int permits_;
+	boost::condition_variable permits_available_;
+public:
+	/**
+	 * Constructor.
+	 *
+	 * @param permits The initial number of permits.
+	 */
+	semaphore(unsigned int permits)
+		: permits_(permits)
+	{
+	}
+
+	/**
+	 * Release a permit.
+	 */
+	void release()
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+
+		++permits_;
+
+		permits_available_.notify_one();
+	}
+
+	/**
+	 * Release a permit.
+	 *
+	 * @param permits The number of permits to release.
+	 */
+	void release(unsigned int permits)
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+
+		permits_ += permits;
+
+		repeat_n(permits, [this] { permits_available_.notify_one(); });
+	}
+
+	/**
+	 * Acquire a permit. Will block until one becomes available if no permit is
+	 * currently available.
+	 */
+	void acquire()
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+
+		while (permits_ == 0u)
+		{
+			permits_available_.wait(lock);
+		}
+
+		--permits_;
+	}
+
+	/**
+	 * Acquire a number of permits. Will block until the given number of
+	 * permits has been acquired if not enough permits are currently available.
+	 *
+	 * @param permits The number of permits to acquire.
+	 */
+	void acquire(unsigned int permits)
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+		auto num_acquired = 0u;
+
+		while (permits_ == 0u && num_acquired < permits)
+		{
+			permits_available_.wait(lock);
+
+			auto num_wanted = permits - num_acquired;
+			auto to_drain = std::min(num_wanted, permits_);
+
+			permits_ -= to_drain;
+			num_acquired += to_drain;
+		}
+	}
+
+	/**
+	 * Acquire one permits if permits are currently available. Does not block
+	 * until one is available, but returns immediately if unavailable.
+	 *
+	 * @return true if a permit was acquired or false if no permits where
+	 *         currently available.
+	 */
+	bool try_acquire()
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+
+		if (permits_ == 0u)
+			return false;
+		else
+		{
+			--permits_;
+
+			return true;
+		}
+	}
+
+	/**
+	 * @return the current number of permits (may have changed at the time of
+	 *         return).
+	 */
+	unsigned int permits() const
+	{
+		boost::mutex::scoped_lock lock(mutex_);
+
+		return permits_;
+	}
+};
+
+}
