071975103b277264457e49ad847000439d2d1a1c
hellgore <hellgore@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-09-19 13:56:58 +0000

    3569308: added 720p2398 and 720p2400 as supported output resolutions (not supported by decklink) and 1080p5994 and 1080p6000 supported by both decklink (the API at least, but hardware may vary) and bluefish
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/trunk@3319 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- core/video_format.cpp
+++ core/video_format.cpp
@@ -53,6 +53,8 @@ const video_format_desc format_descs[video_format::count] =
 	DEFINE_VIDEOFORMATDESC(video_format::pal		,720,  576,  1024, 576,  field_mode::upper,			25,		1,		 boost::assign::list_of(3840),							L"PAL"), 
 	DEFINE_VIDEOFORMATDESC(video_format::ntsc		,720,  486,  720,  540,  field_mode::lower,			30000,	1001,	 boost::assign::list_of(3204)(3202)(3204)(3202)(3204),	L"NTSC"), 
 	DEFINE_VIDEOFORMATDESC(video_format::x576p2500	,1024, 576,  1024, 576,  field_mode::progressive,	25,		1,		 boost::assign::list_of(3840),							L"576p2500"),
+	DEFINE_VIDEOFORMATDESC(video_format::x720p2398	,1280, 720,  1280, 720,  field_mode::progressive,	24000,	1001,	 boost::assign::list_of(4004),							L"720p2398"),
+	DEFINE_VIDEOFORMATDESC(video_format::x720p2400	,1280, 720,  1280, 720,  field_mode::progressive,	24,		1,		 boost::assign::list_of(4000),							L"720p2400"),
 	DEFINE_VIDEOFORMATDESC(video_format::x720p2500	,1280, 720,  1280, 720,  field_mode::progressive,	25,		1,		 boost::assign::list_of(3840),							L"720p2500"), 
 	DEFINE_VIDEOFORMATDESC(video_format::x720p5000	,1280, 720,  1280, 720,  field_mode::progressive,	50,		1,		 boost::assign::list_of(1920),							L"720p5000"), 
 	DEFINE_VIDEOFORMATDESC(video_format::x720p2997	,1280, 720,  1280, 720,  field_mode::progressive,	30000,	1001,	 boost::assign::list_of(3204)(3202)(3204)(3202)(3204),	L"720p2997"),
@@ -68,6 +70,8 @@ const video_format_desc format_descs[video_format::count] =
 	DEFINE_VIDEOFORMATDESC(video_format::x1080p2997	,1920, 1080, 1920, 1080, field_mode::progressive,	30000,	1001,	 boost::assign::list_of(3204)(3202)(3204)(3202)(3204),	L"1080p2997"),
 	DEFINE_VIDEOFORMATDESC(video_format::x1080p3000	,1920, 1080, 1920, 1080, field_mode::progressive,	30,		1,		 boost::assign::list_of(3200),							L"1080p3000"),
 	DEFINE_VIDEOFORMATDESC(video_format::x1080p5000	,1920, 1080, 1920, 1080, field_mode::progressive,	50,		1,		 boost::assign::list_of(1920),							L"1080p5000"),
+	DEFINE_VIDEOFORMATDESC(video_format::x1080p5994	,1920, 1080, 1920, 1080, field_mode::progressive,	60000,	1001,	 boost::assign::list_of(1602)(1601)(1602)(1601)(1602),	L"1080p5994"),
+	DEFINE_VIDEOFORMATDESC(video_format::x1080p6000	,1920, 1080, 1920, 1080, field_mode::progressive,	60,		1,		 boost::assign::list_of(1600),							L"1080p6000"),
 	DEFINE_VIDEOFORMATDESC(video_format::invalid	,0,		0,   0,		0,   field_mode::progressive,	1,		1,		 boost::assign::list_of(1),								L"invalid")
 };
 
