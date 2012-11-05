45dd48d9a175e06bed60e1848b24722f955bd98c
hellgore <hellgore@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-11-05 15:32:39 +0000

    3582474: Server copyright notice
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/server/trunk@3489 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- shell/main.cpp
+++ shell/main.cpp
@@ -129,9 +129,12 @@ void setup_console_window()
 
 void print_info()
 {
-	CASPAR_LOG(info) << L"################################################################################";
-	CASPAR_LOG(info) << L"Copyright (c) 2010 Sveriges Television AB, www.casparcg.com, <info@casparcg.com>";
-	CASPAR_LOG(info) << L"################################################################################";
+	CASPAR_LOG(info) << L"############################################################################";
+	CASPAR_LOG(info) << L"CasparCG Server is distributed by the Swedish Broadcasting Corporation (SVT)";
+	CASPAR_LOG(info) << L"under the GNU General Public License GPLv3 or higher.";
+	CASPAR_LOG(info) << L"Please see LICENSE.TXT for details.";
+	CASPAR_LOG(info) << L"http://www.casparcg.com/";
+	CASPAR_LOG(info) << L"############################################################################";
 	CASPAR_LOG(info) << L"Starting CasparCG Video and Graphics Playout Server " << caspar::env::version();
 	CASPAR_LOG(info) << L"on " << caspar::get_win_product_name() << L" " << caspar::get_win_sp_version();
 	CASPAR_LOG(info) << caspar::get_cpu_info();
