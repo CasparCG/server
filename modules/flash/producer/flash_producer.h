5c02a86d8395cd68925f98bd82c75f7f785d964e
Helge Norberg <helge.norberg@svt.se>
2013-04-10 10:26:24 +0200

    Manually merged 7e84caf001857237cb7a2e13faa3a5e9210e0d32 [formerly 678223b9ae0cc2299f2251c9a3029ed6cf8e441e] from master
    Former-commit-id: 331768319ec72bcc159a88c7d852c30306bad705

--- modules/flash/producer/flash_producer.h
+++ modules/flash/producer/flash_producer.h
@@ -31,6 +31,7 @@
 namespace caspar { namespace flash {
 
 spl::shared_ptr<core::frame_producer> create_producer(const spl::shared_ptr<core::frame_factory>& frame_factory, const core::video_format_desc& format_desc, const std::vector<std::wstring>& params);
+spl::shared_ptr<core::frame_producer> create_swf_producer(const spl::shared_ptr<core::frame_factory>& frame_factory, const core::video_format_desc& format_desc, const std::vector<std::wstring>& params);
 
 std::wstring find_template(const std::wstring& templateName);
 
