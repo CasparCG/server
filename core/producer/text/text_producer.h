4e8fcea63788597bb247cdce4dc190f30b4e4147
niklaspandersson <niklas.p.andersson@svt.se>
2013-08-07 10:03:51 +0200

    * made it optional to normalize coordinates of a text-producer in order for it to work both standalone and in a scene
    Former-commit-id: 9d0f32874d52eb3a0f4e0f87dcd1fa362de22ab6

--- core/producer/text/text_producer.h
+++ core/producer/text/text_producer.h
@@ -46,7 +46,9 @@ namespace caspar { namespace core {
 class text_producer : public frame_producer_base
 {
 public:
-	text_producer(const spl::shared_ptr<frame_factory>& frame_factory, int x, int y, const std::wstring& str, const text::text_info& text_info, long parent_width, long parent_height);
+	text_producer(const spl::shared_ptr<frame_factory>& frame_factory, int x, int y, const std::wstring& str, const text::text_info& text_info, long parent_width, long parent_height, bool standalone);
+	static spl::shared_ptr<text_producer> create(const spl::shared_ptr<class frame_factory>& frame_factory, int x, int y, const std::wstring& str, const text::text_info& text_info, long parent_width, long parent_height, bool standalone = false);
+	
 	draw_frame receive_impl() override;
 	boost::unique_future<std::wstring> call(const std::wstring& param) override;
 
@@ -64,7 +66,6 @@ private:
 	spl::unique_ptr<impl> impl_;
 };
 
-spl::shared_ptr<frame_producer> do_create_text_producer(const spl::shared_ptr<class frame_factory>& frame_factory, int x, int y, const std::wstring& str, const text::text_info& text_info, long parent_width, long parent_height);
 spl::shared_ptr<frame_producer> create_text_producer(const spl::shared_ptr<class frame_factory>& frame_factory, const video_format_desc& format_desc, const std::vector<std::wstring>& params);
 
 
