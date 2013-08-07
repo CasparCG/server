76aca87a45b18d8f813f2898483a90f270f2a2ee
niklaspandersson <niklas.p.andersson@svt.se>
2013-08-07 14:28:27 +0200

    made all producers-factories able to handle non-uppercased params
    Former-commit-id: 948da27680bbdc084890e3dd24f8526c48f27fa7

--- common/param.h
+++ common/param.h
@@ -3,16 +3,31 @@
 #include "except.h"
 
 #include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include <type_traits>
 #include <string>
 
 namespace caspar {
-		
+
+class param_comparer {
+		const std::wstring& lhs;
+	public:
+		explicit param_comparer(const std::wstring& p) : lhs(p) {}
+		bool operator()(const std::wstring& rhs) { return boost::iequals(lhs, rhs); }
+	};
+
+template<typename C>
+bool contains_param(const std::wstring& name, C&& params)
+{
+	return std::find_if(params.begin(), params.end(), param_comparer(name)) != params.end();
+}
+
 template<typename T, typename C>
 typename std::enable_if<!std::is_convertible<T, std::wstring>::value, typename std::decay<T>::type>::type get_param(const std::wstring& name, C&& params, T fail_value = T())
 {	
-	auto it = std::find(std::begin(params), std::end(params), name);
+	auto it = std::find_if(std::begin(params), std::end(params), param_comparer(name));
+	//auto it = std::find(std::begin(params), std::end(params), name);
 	if(it == params.end())	
 		return fail_value;
 	
@@ -32,7 +47,8 @@ typename std::enable_if<!std::is_convertible<T, std::wstring>::value, typename s
 template<typename C>
 std::wstring get_param(const std::wstring& name, C&& params, const std::wstring& fail_value = L"")
 {	
-	auto it = std::find(std::begin(params), std::end(params), name);
+	auto it = std::find_if(std::begin(params), std::end(params), param_comparer(name));
+	//auto it = std::find(std::begin(params), std::end(params), name);
 	if(it == params.end())	
 		return fail_value;
 	
