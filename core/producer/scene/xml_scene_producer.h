724a569d3e47ea46952efb33060768e1f2eb47b0
Helge Norberg <helge.norberg@svt.se>
2013-08-16 14:26:24 +0200

    Initial XML format for scenes and timeline support for PSD

---
+++ core/producer/scene/xml_scene_producer.h
@@ -0,0 +1,39 @@
+/*
+* Copyright (c) 2011 Sveriges Television AB <info@casparcg.com>
+*
+* This file is part of CasparCG (www.casparcg.com).
+*
+* CasparCG is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* CasparCG is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CasparCG. If not, see <http://www.gnu.org/licenses/>.
+*
+* Author: Helge Norberg, helge.norberg@svt.se
+*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <common/memory.h>
+
+namespace caspar { namespace core {
+
+class frame_producer;
+class frame_factory;
+struct video_format_desc;
+
+namespace scene {
+
+spl::shared_ptr<core::frame_producer> create_xml_scene_producer(const spl::shared_ptr<core::frame_factory>& frame_factory, const core::video_format_desc& format_desc, const std::vector<std::wstring>& params);
+
+}}}
