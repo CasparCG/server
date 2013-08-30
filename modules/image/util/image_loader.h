39f8a27770367eb10065d76d0f241ef946589f0f
niklaspandersson <niklas.p.andersson@svt.se>
2013-08-30 13:54:46 +0200

    Manually merged fe983d5 from master
    Former-commit-id: 1bbaf3a97e039f83854b7d0cbfe58086d8f597ab

--- modules/image/util/image_loader.h
+++ modules/image/util/image_loader.h
@@ -29,5 +29,6 @@
 namespace caspar { namespace image {
 
 std::shared_ptr<FIBITMAP> load_image(const std::wstring& filename);
+std::shared_ptr<FIBITMAP> load_png_from_memory(const void* memory_location, size_t size);
 
 }}
