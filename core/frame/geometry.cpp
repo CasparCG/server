caef9a868c87b0d4872353dea2e515b8ad9fdbb8
Helge Norberg <helge.norberg@svt.se>
2013-09-27 12:03:34 +0200

    Changes after feedback by Robert Nagy
    Former-commit-id: 92db9171049f8183ab4ca453237e0d1ca1e3a815

--- core/frame/geometry.cpp
+++ core/frame/geometry.cpp
@@ -28,14 +28,14 @@ namespace caspar { namespace core {
 
 struct frame_geometry::impl
 {
-	impl(frame_geometry::geometry_type t, std::vector<float>&& d) : type_(t), data_(std::move(d)) {}
+	impl(frame_geometry::geometry_type t, std::vector<float> d) : type_(t), data_(std::move(d)) {}
 	
 	frame_geometry::geometry_type type_;
 	std::vector<float> data_;
 };
 
 frame_geometry::frame_geometry() {}
-frame_geometry::frame_geometry(geometry_type t, std::vector<float>&& d) : impl_(new impl(t, std::move(d))) {}
+frame_geometry::frame_geometry(geometry_type t, std::vector<float> d) : impl_(new impl(t, std::move(d))) {}
 
 frame_geometry::geometry_type frame_geometry::type() const { return impl_ ? impl_->type_ : none; }
 const std::vector<float>& frame_geometry::data() const
@@ -49,7 +49,7 @@ const std::vector<float>& frame_geometry::data() const
 const frame_geometry& frame_geometry::get_default()
 {
 	const float d[] = {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-	static frame_geometry g(frame_geometry::quad, std::move(std::vector<float>(d, d+8)));
+	static frame_geometry g(frame_geometry::quad, std::vector<float>(std::begin(d), std::end(d)));
 
 	return g;
 }
