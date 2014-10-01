449ba74af431e37011c601fb2eb9be0927b38e42
Helge Norberg <helge.norberg@svt.se>
2014-10-01 13:32:20 +0200

    build scripts

--- build-scripts/set-variables-and-build-windows.bat
+++ build-scripts/set-variables-and-build-windows.bat
@@ -4,5 +4,8 @@ set BUILD_VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvar
 set BUILD_ARCHIVE_NAME=CasparCG Server
 set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
 
+:: Uncomment if using Visual Studio Express. And install WDK 7.1.0
+::set BUILD_ATL_INCLUDE_PATH=BUILD_ATL_INCLUDE_PATH
+
 build-windows.bat
 
