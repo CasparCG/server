f6e55ee86149dae7f26982cd1d9954254bc36ae4
Helge Norberg <helge.norberg@svt.se>
2015-03-05 22:10:47 +0100

    OGL image_mixer: Fixed a performance hotspot where an idle channel without any layers performs zero initialization of memory for each frame. Now uses a shared memory buffer as big as the biggest video mode available for all channels.

--- common/enum_class.h
+++ common/enum_class.h
@@ -2,6 +2,9 @@
 
 #include <type_traits>
 
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/irange.hpp>
+
 // Macro that defines & and &= for an enum class. Add more when needed.
 
 #define ENUM_ENABLE_BITWISE(enum_class) \
@@ -16,3 +19,26 @@
 		lhs = lhs & rhs; \
 		return lhs; \
 	};
+
+namespace caspar {
+
+template<typename E>
+struct enum_from_int
+{
+	typedef E result_type;
+
+	E operator()(typename std::underlying_type<E>::type i) const
+	{
+		return static_cast<E>(i);
+	}
+};
+
+// For enum classes starting at 0 and without any gaps with a terminating count constant.
+template <typename E>
+boost::transformed_range<enum_from_int<E>, const boost::integer_range<typename std::underlying_type<E>::type>> iterate_enum()
+{
+	typedef typename std::underlying_type<E>::type integer;
+	return boost::irange(static_cast<integer>(0), static_cast<integer>(E::count)) | boost::adaptors::transformed(enum_from_int<E>());
+}
+
+}
