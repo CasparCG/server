25d5cac6e084b6f74f5395863e6df77b59d71e90
Helge Norberg <helge.norberg@svt.se>
2015-05-21 21:56:18 +0200

    * Build script for Windows.

--- build-scripts/set-variables-and-build-windows.bat
+++ build-scripts/set-variables-and-build-windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set BUILD_VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat
+set BUILD_VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
 set BUILD_ARCHIVE_NAME=CasparCG Server
 set BUILD_PARALLEL_THREADS=12
 set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
