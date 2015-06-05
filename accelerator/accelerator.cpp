d6a95b253a0b2a06b2aa306ac88d3444a6f84fb7
Helge Norberg <helge.norberg@svt.se>
2015-06-05 16:00:09 +0200

    * Merged mipmapping_defaul_on configuration property.

--- accelerator/accelerator.cpp
+++ accelerator/accelerator.cpp
@@ -37,7 +37,7 @@ struct accelerator::impl
 				if(!ogl_device_)
 					ogl_device_.reset(new ogl::device());
 
-				return std::unique_ptr<core::image_mixer>(new ogl::image_mixer(spl::make_shared_ptr(ogl_device_), env::properties().get(L"configuration.blend-modes", false)));
+				return std::unique_ptr<core::image_mixer>(new ogl::image_mixer(spl::make_shared_ptr(ogl_device_), env::properties().get(L"configuration.mixer.blend-modes", false)));
 			}
 		}
 		catch(...)
