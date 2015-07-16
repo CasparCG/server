4e70a330985a8d7e367fa4b3d9af8d301d035e7a
Daniel Roviriego <danifernando@gmail.com>
2015-07-16 17:21:51 -0300

    ask for user to input the number of threads/cores for compilling when building with GNU/Linux

--- build-scripts/set-variables-and-build-linux.sh
+++ build-scripts/set-variables-and-build-linux.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+
+# set threads for compilling
+echo "How many cores do you want to use for compilling : "
+read BUILD_PARALLEL_THREADS_NUMBER
+echo "number of cores to compile is: $BUILD_PARALLEL_THREADS_NUMBER"
+
 export BUILD_ARCHIVE_NAME="CasparCG Server"
-export BUILD_PARALLEL_THREADS=12
+export BUILD_PARALLEL_THREADS=$BUILD_PARALLEL_THREADS_NUMBER
 
 ./build-linux.sh
 
