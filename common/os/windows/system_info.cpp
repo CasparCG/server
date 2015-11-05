8623ff013d63c9e7f39a1b086d0f32799562b3bc
Helge Norberg <helge.norberg@svt.se>
2015-11-05 12:12:23 +0100

    * Moved functions from windows/current_version.h to windows/system_info.cpp and removed windows/current_version.h because it was only used by windows/system_info.cpp anyway

--- common/os/windows/system_info.cpp
+++ common/os/windows/system_info.cpp
@@ -22,7 +22,6 @@
 #include "../../stdafx.h"
 
 #include "../system_info.h"
-#include "current_version.h"
 
 #include "windows.h"
 
@@ -79,6 +78,46 @@ std::wstring system_product_name()
 	}
 
 	return system_product_name;
+}
+
+std::wstring win_product_name()
+{
+	std::wstring result = L"Unknown Windows Product Name.";
+	HKEY hkey;
+	DWORD dwType, dwSize;
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"), 0, KEY_QUERY_VALUE, &hkey) == ERROR_SUCCESS)
+	{
+		wchar_t p_name_str[1024];
+
+		dwType = REG_SZ;
+		dwSize = sizeof(p_name_str);
+
+		if (RegQueryValueEx(hkey, TEXT("ProductName"), NULL, &dwType, (PBYTE)&p_name_str, &dwSize) == ERROR_SUCCESS)
+			result = p_name_str;
+
+		RegCloseKey(hkey);
+	}
+	return result;
+}
+
+std::wstring win_sp_version()
+{
+	std::wstring result = L"";
+	HKEY hkey;
+	DWORD dwType, dwSize;
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"), 0, KEY_QUERY_VALUE, &hkey) == ERROR_SUCCESS)
+	{
+		wchar_t csd_ver_str[1024];
+
+		dwType = REG_SZ;
+		dwSize = sizeof(csd_ver_str);
+
+		if (RegQueryValueEx(hkey, TEXT("CSDVersion"), NULL, &dwType, (PBYTE)&csd_ver_str, &dwSize) == ERROR_SUCCESS)
+			result = csd_ver_str;
+
+		RegCloseKey(hkey);
+	}
+	return result;
 }
 
 std::wstring os_description()
