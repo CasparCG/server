d80727756104359700ef1b6079abc9abdea78e42
Helge Norberg <helge.norberg@svt.se>
2015-11-16 19:15:07 +0100

    Included libcef.dll.pdb to get stack traces for CEF code (at least temporarily while we are in the testing phase of 2.1). Changed compression format to 7Zip instead of Zip to reduce the build size to approximately the same as before. Unpacked libcef.dll.pdb is about 500 MB though, which is quite large.
    Former-commit-id: 9e39d6a2c23e7fc539706fe83196e1ae8ceeae8b [formerly 70b97ec83345864658827e020149462df752d598]
    Former-commit-id: 42b5fc52fe269ba4fc7c3cdc0fc73bc5835d2e58

--- build-scripts/build-windows.bat
+++ build-scripts/build-windows.bat
@@ -45,6 +45,7 @@ echo Copying binaries...
 copy shell\*.dll "%SERVER_FOLDER%\Server" || goto :error
 copy shell\RelWithDebInfo\casparcg.exe "%SERVER_FOLDER%\Server" || goto :error
 copy shell\RelWithDebInfo\casparcg.pdb "%SERVER_FOLDER%\Server" || goto :error
+copy shell\RelWithDebInfo\libcef.dll.pdb "%SERVER_FOLDER%\Server" || goto :error
 copy shell\casparcg.config "%SERVER_FOLDER%\Server" || goto :error
 copy shell\*.ttf "%SERVER_FOLDER%\Server" || goto :error
 xcopy shell\locales "%SERVER_FOLDER%\Server\locales" /E /I /Y || goto :error
@@ -57,7 +58,7 @@ copy ..\README "%SERVER_FOLDER%" || goto :error
 
 :: Create zip file
 echo Creating zip...
-"%BUILD_7ZIP%" a "%BUILD_ARCHIVE_NAME%.zip" "%SERVER_FOLDER%" || goto :error
+"%BUILD_7ZIP%" a "%BUILD_ARCHIVE_NAME%.7z" "%SERVER_FOLDER%" || goto :error
 
 :: Skip exiting with failure
 goto :EOF
