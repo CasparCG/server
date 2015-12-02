dd31df1a1ff4e2d0cd22b6a32a58d6d2d03c3d51
Helge Norberg <helge.norberg@svt.se>
2015-12-02 17:25:46 +0100

    Created a context_info attachment to our exceptions with information about the context in which the exception was thrown. Each part of the code can at any time contribute to the context using a scoped_context, literally appending information to the thread of execution
    Former-commit-id: 9b1484931094a9d8c5e8764b3989fd1647075e02 [formerly 9b1484931094a9d8c5e8764b3989fd1647075e02 [formerly 2d1fd273a2ea0a43642518c84497523a4ea49f34]]
    Former-commit-id: 0cbfe0d9106592c77e157d762cc71054bc8e941f
    Former-commit-id: 78626d245775c768718c6d01fac12c948f2151ea

---
+++ common/except.cpp
@@ -0,0 +1,115 @@
+/*
+* Copyright (c) 2011 Sveriges Television AB <info@casparcg.com>
+*
+* This file is part of CasparCG (www.casparcg.com).
+*
+* CasparCG is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* CasparCG is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CasparCG. If not, see <http://www.gnu.org/licenses/>.
+*
+* Author: Robert Nagy, ronag89@gmail.com
+*/
+
+#include "stdafx.h"
+
+#include "except.h"
+
+#include <boost/thread/tss.hpp>
+#include <boost/algorithm/string/join.hpp>
+
+namespace {
+
+boost::thread_specific_ptr<std::list<std::string>>& context_stacks_per_thread()
+{
+	static boost::thread_specific_ptr<std::list<std::string>> instances;
+
+	return instances;
+}
+
+}
+
+namespace caspar {
+
+std::list<std::string>& context_stack_for_thread()
+{
+	auto local = context_stacks_per_thread().get();
+
+	if (!local)
+	{
+		local = new std::list<std::string>();
+		context_stacks_per_thread().reset(local);
+	}
+
+	return *local;
+}
+
+std::string get_context()
+{
+	return boost::join(context_stack_for_thread(), "");
+}
+
+scoped_context::scoped_context()
+	: scoped_context::scoped_context("")
+{
+}
+
+scoped_context::scoped_context(std::string msg)
+	: for_thread_(context_stack_for_thread())
+{
+	for_thread_.push_back(std::move(msg));
+	msg_ = &for_thread_.back();
+}
+
+void scoped_context::replace_msg(std::string msg)
+{
+	if (&for_thread_ != &context_stack_for_thread())
+		CASPAR_THROW_EXCEPTION(invalid_operation() << msg_info("Called from wrong thread"));
+
+	*msg_ = std::move(msg);
+}
+
+void scoped_context::clear_msg()
+{
+	replace_msg("");
+}
+
+scoped_context::~scoped_context()
+{
+	for_thread_.pop_back();
+}
+
+
+
+std::string get_message_and_context(const caspar_exception& e)
+{
+	std::string result;
+
+	auto msg = boost::get_error_info<msg_info_t>(e);
+	auto ctx = boost::get_error_info<context_info_t>(e);
+
+	if (msg)
+		result += *msg;
+
+	if (ctx && !ctx->empty())
+	{
+		result += " (";
+		result += *ctx;
+		result += ")";
+	}
+
+	if (!result.empty() && result.back() != '.')
+		result += ".";
+
+	return result;
+}
+
+}
