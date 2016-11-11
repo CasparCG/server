b17a5351a13075978b5e5d4c817fb683224d9d38
Helge Norberg <helge.norberg@svt.se>
2016-11-11 21:25:29 +0100

    [ffmpeg_consumer] Retired old implementation in favour of the now updated streaming_consumer

--- modules/ffmpeg/ffmpeg.cpp
+++ modules/ffmpeg/ffmpeg.cpp
@@ -24,7 +24,6 @@
 #include "ffmpeg.h"
 
 #include "consumer/ffmpeg_consumer.h"
-#include "consumer/streaming_consumer.h"
 #include "producer/ffmpeg_producer.h"
 #include "producer/util/util.h"
 
@@ -253,10 +252,8 @@ void init(core::module_dependencies dependencies)
 
 	auto info_repo = dependencies.media_info_repo;
 
-	dependencies.consumer_registry->register_consumer_factory(L"FFmpeg Consumer", create_consumer, describe_consumer);
-	dependencies.consumer_registry->register_consumer_factory(L"Streaming Consumer", create_streaming_consumer, describe_streaming_consumer);
-	dependencies.consumer_registry->register_preconfigured_consumer_factory(L"file", create_preconfigured_consumer);
-	dependencies.consumer_registry->register_preconfigured_consumer_factory(L"stream", create_preconfigured_streaming_consumer);
+	dependencies.consumer_registry->register_consumer_factory(L"FFmpeg Consumer", create_ffmpeg_consumer, describe_ffmpeg_consumer);
+	dependencies.consumer_registry->register_preconfigured_consumer_factory(L"ffmpeg", create_preconfigured_ffmpeg_consumer);
 	dependencies.producer_registry->register_producer_factory(L"FFmpeg Producer", boost::bind(&create_producer, _1, _2, info_repo), describe_producer);
 	dependencies.producer_registry->register_thumbnail_producer(boost::bind(&create_thumbnail_frame, _1, _2, info_repo));
 
