6e067cb6fdb748dfd81e86b15f01aac2e107daed
walterav1984 <walterav1984@gmail.com>
2017-01-29 13:25:07 +0100

    fix download of zlib by updating URL

--- dependencies64/ffmpeg/ffmpeg-build-linux/build.sh
+++ dependencies64/ffmpeg/ffmpeg-build-linux/build.sh
@@ -40,7 +40,7 @@ mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 echo "#### FFmpeg static build, by STVS SA ####"
 cd $BUILD_DIR
 ../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
-../fetchurl "http://zlib.net/zlib-1.2.8.tar.gz"
+../fetchurl "http://www.zlib.net/fossils/zlib-1.2.8.tar.gz"
 ../fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
 #../fetchurl "http://downloads.sf.net/project/libpng/libpng15/older-releases/1.5.14/libpng-1.5.14.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz"
