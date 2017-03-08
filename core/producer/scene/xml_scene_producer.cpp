980ef30b4065a39f6b714a01a1e973cc4d38442d
Helge Norberg <helge.norberg@svt.se>
2017-03-08 20:02:27 +0100

    [scene] Fixed wrong framerate of nested content when channel is interlaced.

--- core/producer/scene/xml_scene_producer.cpp
+++ core/producer/scene/xml_scene_producer.cpp
@@ -141,8 +141,10 @@ spl::shared_ptr<core::frame_producer> create_xml_scene_producer(
 
 			adjusted_format_desc.field_mode		 = field_mode::progressive;
 			adjusted_format_desc.fps			*= adjusted_format_desc.field_count;
-			adjusted_format_desc.duration		/= adjusted_format_desc.field_count;
+			adjusted_format_desc.time_scale		*= adjusted_format_desc.field_count;
+			adjusted_format_desc.framerate		*= adjusted_format_desc.field_count;
 			adjusted_format_desc.field_count	 = 1;
+			adjusted_format_desc.audio_cadence	 = core::find_audio_cadence(adjusted_format_desc.framerate);
 
 			return dependencies.producer_registry->create_producer(adjusted_dependencies, producer_string);
 		}();
