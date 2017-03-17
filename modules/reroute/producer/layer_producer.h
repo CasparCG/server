d494a159ca57b1c7a9ce24bcf13246702428a0ee
Helge Norberg <helge.norberg@svt.se>
2017-03-17 17:41:11 +0100

    [reroute] Fixed serious out of memory situation where too many audio samples are queued. This also has the side effect of mismatching framerates between routed from and routed to channels work. Also audio is always routed, instead of only when framerate matches.
    Former-commit-id: ea1e66a364c7fe67b984bbc7f3b7c80375737e6a

--- modules/reroute/producer/layer_producer.h
+++ modules/reroute/producer/layer_producer.h
@@ -27,6 +27,10 @@
 
 namespace caspar { namespace reroute {
 
-spl::shared_ptr<core::frame_producer> create_layer_producer(const spl::shared_ptr<core::video_channel>& channel, int layer, int frames_delay);
+spl::shared_ptr<core::frame_producer> create_layer_producer(
+		const spl::shared_ptr<core::video_channel>& channel,
+		int layer,
+		int frames_delay,
+		const core::video_format_desc& destination_mode);
 
 }}
