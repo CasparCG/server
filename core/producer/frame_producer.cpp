c798aea3adaee9e4e2ecf3992b12cc40dbd685d7
Julian Waller <git@julusian.co.uk>
2017-10-28 17:29:54 +0100

    [html_producer] Allow running html templates from an arbitrary url rather than only from template folder
    # Conflicts:
    #	core/producer/frame_producer.cpp
    #	modules/html/producer/html_producer.cpp
    
    
    Former-commit-id: 08d3a637db41c52dc87b813bda852f24d423efca [formerly e96302053f04e32558652b80486f4aac820d0ad2]
    Former-commit-id: ef9ed4a9fc2567ccd306bcaff8b1f09c2a1f488f

--- core/producer/frame_producer.cpp
+++ core/producer/frame_producer.cpp
@@ -156,7 +156,10 @@ const spl::shared_ptr<frame_producer>& frame_producer::empty()
 		monitor::subject& monitor_output() override {static monitor::subject monitor_subject(""); return monitor_subject;}
 		std::wstring name() const override {return L"empty";}
 		uint32_t frame_number() const override {return 0;}
-		std::future<std::wstring> call(const std::vector<std::wstring>& params) override{CASPAR_THROW_EXCEPTION(not_implemented());}
+		std::future<std::wstring> call(const std::vector<std::wstring>& params) override{
+			CASPAR_LOG(warning) << L" Cannot call on empty frame_producer";
+			return make_ready_future(std::wstring(L""));
+		}
 		draw_frame last_frame() {return draw_frame::empty();}
 	};
 
