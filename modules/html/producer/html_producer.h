2ff49fe49104e2e3abefd6d14c393ae89e2891a8
Julian Waller <git@julusian.co.uk>
2017-10-28 17:29:54 +0100

    [html_producer] Allow running html templates from an arbitrary url rather than only from template folder

--- modules/html/producer/html_producer.h
+++ modules/html/producer/html_producer.h
@@ -34,5 +34,8 @@ void describe_producer(core::help_sink& sink, const core::help_repository& repo)
 spl::shared_ptr<core::frame_producer> create_producer(
 		const core::frame_producer_dependencies& dependencies,
 		const std::vector<std::wstring>& params);
+spl::shared_ptr<core::frame_producer> create_cg_producer(
+	const core::frame_producer_dependencies& dependencies,
+	const std::vector<std::wstring>& params);
 
 }}
