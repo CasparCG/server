d2a13f220d12ad00a53148893b1be6f48e9a52d8
Robert Nagy <ronagy@icloud.com>
2018-01-27 14:59:21 +0100

    More OpenGL refactoring
    Former-commit-id: 10a0f925d81604590537ec34bfea4270c0f661c6

---
+++ common/asio.h
@@ -0,0 +1,43 @@
+#include <boost/asio/post.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/spawn.hpp>
+
+namespace caspar {
+
+template<typename C, typename Func>
+auto dispatch_async(C& context, Func&& func)
+{
+	typedef decltype(func())                  result_type;
+	typedef std::packaged_task<result_type()> task_type;
+
+    auto task = task_type(std::forward<Func>(func));
+    auto future = task.get_future();
+    boost::asio::dispatch(context, task_type(std::forward<Func>(func)));
+    return fold(future);
+}
+
+template<typename C, typename Func>
+auto post_async(C& context, Func&& func)
+{
+	typedef decltype(func())                  result_type;
+	typedef std::packaged_task<result_type()> task_type;
+
+    auto task = task_type(std::forward<Func>(func));
+    auto future = task.get_future();
+    boost::asio::post(context, task_type(std::forward<Func>(func)));
+    return fold(future);
+}
+
+template<typename C, typename Func>
+auto spawn_async(C& context, Func&& func)
+{
+	typedef decltype(func(std::declval<yield_context>()))  result_type;
+	typedef std::packaged_task<result_type(yield_context)> task_type;
+
+    auto task = task_type(std::forward<Func>(func));
+    auto future = task.get_future();
+    boost::asio::spawn(std::move(task));
+    return fold(future);
+}
+
+}
