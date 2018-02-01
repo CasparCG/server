8b4f173439b1be34bdd4d7bf446a240a8d88a5fc
ronagy <ronagy@icloud.com>
2018-02-01 12:05:20 +0100

    ffmpeg: move make_frame into util
    Former-commit-id: b9de5f3e4342459e0b25adbd65892dbda9b409b0

--- modules/ffmpeg/producer/av_util.h
+++ modules/ffmpeg/producer/av_util.h
@@ -1,10 +1,14 @@
 #include <core/frame/pixel_format.h>
+#include <core/frame/frame.h>
+#include <core/frame/frame_factory.h>
 
+#include <memory>
 enum AVPixelFormat;
 
 namespace caspar { namespace ffmpeg {
 
 core::pixel_format get_pixel_format(AVPixelFormat pix_fmt);
 core::pixel_format_desc pixel_format_desc(AVPixelFormat pix_fmt, int width, int height);
+core::mutable_frame make_frame(void* tag, core::frame_factory& frame_factory, std::shared_ptr<AVFrame> video, std::shared_ptr<AVFrame> audio);
 
 }}
