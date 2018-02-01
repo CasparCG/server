c50b9831f0d3ab6e0f95aabb2795330b23556917
ronagy <ronagy@icloud.com>
2018-02-01 12:04:09 +0100

    ffmpeg: cleanup incldues
    Former-commit-id: 4e4fb04c685c2c6d9e7a9f06e1a868776a10f9ce

--- modules/ffmpeg/StdAfx.h
+++ modules/ffmpeg/StdAfx.h
@@ -21,73 +21,69 @@
 
 #pragma once
 
-#if defined _DEBUG && defined _MSC_VER
-#include <crtdbg.h>
-#endif
-
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-
 #include <algorithm>
-#include <array>
-#include <assert.h>
-#include <boost/algorithm/string.hpp>
-#include <boost/algorithm/string/case_conv.hpp>
+#include <atomic>
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/exception/all.hpp>
+#include <boost/exception/exception.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/format.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/noncopyable.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/range/adaptors.hpp>
-#include <boost/range/algorithm.hpp>
-#include <boost/range/algorithm.hpp>
-#include <boost/range/algorithm/find.hpp>
-#include <boost/range/algorithm/find_if.hpp>
-#include <boost/range/algorithm_ext/push_back.hpp>
-#include <boost/range/iterator_range.hpp>
-#include <boost/regex.hpp>
-#include <cstdint>
-#include <cstdio>
+#include <boost/optional.hpp>
+#include <boost/range/algorithm/rotate.hpp>
+#include <boost/rational.hpp>
+#include <cinttypes>
+#include <common/diagnostics/graph.h>
+#include <common/env.h>
+#include <common/except.h>
+#include <common/log.h>
+#include <common/memory.h>
+#include <common/os/filesystem.h>
+#include <common/param.h>
+#include <common/scope_exit.h>
+#include <condition_variable>
+#include <core/consumer/frame_consumer.h>
+#include <core/frame/draw_frame.h>
+#include <core/frame/frame.h>
+#include <core/frame/frame_factory.h>
+#include <core/frame/pixel_format.h>
+#include <core/fwd.h>
+#include <core/module_dependencies.h>
+#include <core/producer/frame_producer.h>
+#include <core/video_format.h>
 #include <deque>
-#include <deque>
+#include <exception>
 #include <functional>
-#include <iostream>
-#include <limits>
-#include <math.h>
 #include <memory>
+#include <mutex>
 #include <queue>
-#include <sstream>
+#include <set>
 #include <string>
-#include <tbb/cache_aligned_allocator.h>
 #include <tbb/concurrent_queue.h>
-#include <tbb/concurrent_unordered_map.h>
-#include <tbb/parallel_for.h>
-#include <tbb/parallel_invoke.h>
-#include <tbb/recursive_mutex.h>
-#include <tbb/tbb_thread.h>
-#include <unordered_map>
+#include <thread>
 #include <vector>
-#include <common/timer.h>
 
 #pragma warning(push, 1)
 
 extern "C"
 {
-	#define __STDC_CONSTANT_MACROS
-	#define __STDC_LIMIT_MACROS
-	#include <libavcodec/avcodec.h>
-	#include <libavfilter/avfilter.h>
-	#include <libavfilter/buffersink.h>
-	#include <libavformat/avformat.h>
-	#include <libavutil/avutil.h>
-	#include <libavutil/common.h>
-	#include <libavutil/cpu.h>
-	#include <libavutil/error.h>
-	#include <libavutil/imgutils.h>
-	#include <libavutil/opt.h>
-	#include <libavutil/pixdesc.h>
-	#include <libavutil/samplefmt.h>
-	#include <libswscale/swscale.h>
+#define __STDC_CONSTANT_MACROS
+#define __STDC_LIMIT_MACROS
+#include <libavdevice/avdevice.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswscale/swscale.h>
+#include <libavcodec/avcodec.h>
+#include <libavfilter/avfilter.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavformat/avformat.h>
+#include <libavutil/frame.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixfmt.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/timecode.h>
 }
 
 #pragma warning(pop)
