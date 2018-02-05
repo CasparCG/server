5796e1c4e2d15d4198476faddc7cd6e159a43e42
Robert Nagy <ronagy@icloud.com>
2018-02-06 00:07:58 +0100

    refactor(node): modularize
    Former-commit-id: ccf157f3bfa760468b1b25f3c4081e4487bd16d7 [formerly afbea197ce8cbe79d1cc171fe23537d203af995e]
    Former-commit-id: 95ba17fa3b56ec921707cb763dac160a0d6dcc99

---
+++ node/src/scanner.js
@@ -0,0 +1,149 @@
+
+const cp = require('child_process')
+const { Observable } = require('@reactivex/rxjs')
+const util = require('util')
+const chokidar = require('chokidar')
+const mkdirp = require('mkdirp-promise')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const { getId } = require('./util')
+
+const statAsync = util.promisify(fs.stat)
+const unlinkAsync = util.promisify(fs.unlink)
+const readFileAsync = util.promisify(fs.readFile)
+
+module.exports = function ({ config, db, logger }) {
+  const watcher = chokidar
+    .watch(config.paths.media, {
+      alwaysStat: true,
+      awaitWriteFinish: {
+        stabilityThreshold: 2000,
+        pollInterval: 100
+      },
+      ...config.thumbnails.chokidar
+    })
+    .on('error', err => logger.error({ err }))
+
+  Observable
+    .merge(
+      Observable.fromEvent(watcher, 'add'),
+      Observable.fromEvent(watcher, 'change'),
+      Observable.fromEvent(watcher, 'unlink')
+    )
+    .concatMap(async ([ mediaPath, mediaStat ]) => {
+      const mediaId = getId(mediaPath)
+      try {
+        await scanFile(mediaPath, mediaId, mediaStat)
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          await db.delete(mediaId)
+        } else {
+          logger.error({ err })
+        }
+      }
+    })
+    .subscribe()
+
+  async function scanFile (mediaPath, mediaId, mediaStat) {
+    const doc = await db
+      .get(mediaId)
+      .catch(() => ({
+        _id: mediaId,
+        mediaPath: mediaPath
+      }))
+
+    if (doc.mediaSize !== mediaStat.size || doc.mediaTime !== mediaStat.mtime.getTime()) {
+      return
+    }
+
+    doc.mediaSize = mediaStat.size
+    doc.mediaTime = mediaStat.mtime.getTime()
+
+    await Promise.all([
+      generateInfo(doc).catch(err => {
+        logger.error({ err })
+      }),
+      generateThumb(doc).catch(err => {
+        logger.error({ err })
+      })
+    ])
+
+    await db.put(doc)
+  }
+
+  async function generateThumb (doc) {
+    const tmpPath = path.join(os.tmpdir(), Math.random().toString(16))
+
+    const args = [
+      // TODO (perf) Low priority process?
+      config.paths.ffmpeg,
+      '-hide_banner',
+      '-i', `"${doc.mediaPath}"`,
+      '-frames:v 1',
+      `-vf thumbnail,scale=${config.thumbnails.width}:${config.thumbnails.height}`,
+      '-f png',
+      '-threads 1',
+      tmpPath
+    ]
+
+    await mkdirp(path.dirname(tmpPath))
+    await new Promise((resolve, reject) => {
+      cp.exec(args.join(' '), (err, stdout, stderr) => err ? reject(err) : resolve())
+    })
+
+    const thumbStat = await statAsync(tmpPath)
+    doc.thumbSize = thumbStat.size
+    doc.thumbTime = thumbStat.mtime.toISOString()
+    doc.tinf = [
+      `"${getId(config.paths.media, doc.mediaPath)}"`,
+      // TODO (fix) Binary or base64 size?
+      doc.thumbSize
+    ].join(' ') + '\r\n'
+
+    await db.putAttachment(doc._id, 'thumb.png', await readFileAsync(tmpPath), 'image/png')
+    await unlinkAsync(tmpPath)
+  }
+
+  async function generateInfo (doc) {
+    doc.cinf = await new Promise((resolve, reject) => {
+      const args = [
+        // TODO (perf) Low priority process?
+        config.paths.ffprobe,
+        '-hide_banner',
+        '-i', `"${doc.mediaPath}"`,
+        '-show_format',
+        '-show_streams',
+        '-print_format', 'json'
+      ]
+      cp.exec(args.join(' '), (err, stdout, stderr) => {
+        if (err) {
+          return reject(err)
+        }
+
+        const json = JSON.parse(stdout)
+        if (!json.format || !json.streams || !json.streams[0]) {
+          return reject(new Error('not media'))
+        }
+
+        const timeBase = json.streams[0].time_base
+        const duration = json.streams[0].duration_ts
+
+        let type = 'AUDIO'
+        if (duration <= 1) {
+          type = 'STILL'
+        } else if (json.streams[0].pix_fmt) {
+          type = 'MOVIE'
+        }
+
+        resolve([
+          `"${getId(config.paths.media, doc.mediaPath)}"`,
+          type,
+          doc.mediaSize,
+          duration,
+          timeBase
+        ].join(' '))
+      })
+    })
+  }
+}
