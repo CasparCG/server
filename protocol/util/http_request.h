c286c3a6949eefa981499617ae0b43f30445fa1a
ronagy <ronagy@icloud.com>
2018-02-05 18:43:41 +0100

    fix: missing files

---
+++ protocol/util/http_request.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+namespace caspar {
+namespace http {
+
+struct HTTPResponse
+{
+    unsigned int                       status_code;
+    std::string                        status_message;
+    std::map<std::string, std::string> headers;
+    std::string                        body;
+};
+
+HTTPResponse request(const std::string& host, const std::string& port, const std::string& path);
+
+std::string url_encode(const std::string& str);
+
+}
+}
