c905f11f73b119ecd0d1ecdaca41470cb2460f62
ronagy <ronagy@icloud.com>
2018-02-05 08:47:02 +0100

    fix(common/future): cleanup and fix template deduction

--- common/future.h
+++ common/future.h
@@ -1,17 +1,16 @@
 #pragma once
 
-#include <boost/function.hpp>
-#include <boost/optional.hpp>
-
-#include <functional>
+#include <chrono>
 #include <future>
+#include <type_traits>
+#include <utility>
 
 namespace caspar {
 
 template <typename F>
 auto flatten(F&& f)
 {
-    return std::async(std::launch::deferred, [](F&& f) { return f.get().get(); }, std::forward<F>(f));
+    return std::async(std::launch::deferred, [f = std::forward<F>(f)] () mutable { return f.get().get(); });
 }
 
 template <typename F>
@@ -20,32 +19,18 @@ bool is_ready(const F& future)
     return future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 }
 
-/**
- * Wrap a value in a future with an already known result.
- * <p>
- * Useful when the result of an operation is already known at the time of
- * calling.
- *
- * @param value The r-value to wrap.
- *
- * @return The future with the result set.
- */
-template <class R>
-std::future<R> make_ready_future(R&& value)
+template <class T>
+auto make_ready_future(T&& value)
 {
-    std::promise<R> p;
-
-    p.set_value(value);
-
+    std::promise<typename std::decay<T>::type> p;
+    p.set_value(std::forward<T>(value));
     return p.get_future();
 }
 
 static std::future<void> make_ready_future()
 {
     std::promise<void> p;
-
     p.set_value();
-
     return p.get_future();
 }
 
