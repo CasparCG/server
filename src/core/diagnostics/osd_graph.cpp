dfa9264ca82ce7e8868fda3889ae6fc87023c680
ronagy <ronagy@icloud.com>
2018-02-06 17:28:59 +0100

    removed unused fonts
    Former-commit-id: 350c3c43f7721fb732a3680dc65eb95d058c4202

--- src/core/diagnostics/osd_graph.cpp
+++ src/core/diagnostics/osd_graph.cpp
@@ -69,7 +69,7 @@ sf::Font& get_default_font()
 {
     static sf::Font DEFAULT_FONT = []() {
         sf::Font font;
-        if (!font.loadFromFile("LiberationSans-Regular.ttf"))
+        if (!font.loadFromFile("LiberationMono-Regular.ttf"))
             CASPAR_THROW_EXCEPTION(file_not_found() << msg_info("LiberationSans-Regular.ttf not found"));
         return font;
     }();
