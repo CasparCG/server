c9d4a2231622f2e93ed462cb11bcb64e927cb142
Robert Nagy <ronagy@icloud.com>
2018-02-07 09:06:30 +0100

    fix: build scripts
    Former-commit-id: 0ae8b84b82fc6894aa5e85046a45e46c403b4db6

--- build-scripts/windows-vs2015/set-variables-and-build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set BUILD_VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
-set BUILD_ARCHIVE_NAME=CasparCG Server
-set BUILD_PARALLEL_THREADS=12
-set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
-
-build-scripts\windows-vs2015\build.bat
-
