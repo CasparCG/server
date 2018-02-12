ce3299161b01aeed05ea06422bdca09a7d32b369
ronagy <ronagy@icloud.com>
2018-02-12 17:26:50 +0100

    fix(ffmpeg): update graph after loop

--- src/modules/ffmpeg/producer/av_input.cpp
+++ src/modules/ffmpeg/producer/av_input.cpp
@@ -109,8 +109,8 @@ void Input::operator()(std::function<bool(std::shared_ptr<AVPacket>&)> fn)
         std::unique_lock<std::mutex> lock(mutex_);
         while (!output_.empty() && fn(output_.front())) {
             output_.pop();
-            graph_->set_value("input", (static_cast<double>(output_.size() + 0.001) / output_capacity_));
         }
+        graph_->set_value("input", (static_cast<double>(output_.size() + 0.001) / output_capacity_));
     }
     cond_.notify_all();
 }
