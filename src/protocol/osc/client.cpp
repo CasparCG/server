05931990152842b39c69f94d5de9e12f1ac1deb7
ronag <ronag89@gmail.com>
2018-03-13 14:35:21 +0100

    fix(osc): async

--- src/protocol/osc/client.cpp
+++ src/protocol/osc/client.cpp
@@ -34,8 +34,12 @@
 #include <core/monitor/monitor.h>
 
 #include <boost/asio.hpp>
+#include <boost/optional.hpp>
 
+#include <mutex>
+#include <condition_variable>
 #include <vector>
+#include <thread>
 
 using namespace boost::asio::ip;
 
@@ -66,9 +70,14 @@ struct client::impl : public spl::enable_shared_from_this<client::impl>
 {
     std::shared_ptr<boost::asio::io_context> service_;
     udp::socket                              socket_;
-    std::mutex                               endpoints_mutex_;
     std::map<udp::endpoint, int>             reference_counts_by_endpoint_;
     std::vector<char>                        buffer_;
+
+    std::mutex                mutex_;
+    std::condition_variable   cond_;
+    boost::optional<core::monitor::data_map_t> bundle_opt_;
+    std::atomic<bool>         abort_request_{ false };
+    std::thread               thread_;
 
   public:
     impl(std::shared_ptr<boost::asio::io_service> service)
@@ -77,12 +86,72 @@ struct client::impl : public spl::enable_shared_from_this<client::impl>
         // TODO (fix) Split into smaller packets.
         , buffer_(1000000)
     {
+        thread_ = std::thread([=] {
+            try {
+                while (!abort_request_) {
+                    core::monitor::data_map_t bundle;
+                    std::vector<udp::endpoint> endpoints;
+
+                    {
+                        std::unique_lock<std::mutex> lock(mutex_);
+                        cond_.wait(lock, [&] { return bundle_opt_ || abort_request_;  });
+
+                        if (abort_request_) {
+                            return;
+                        }
+
+                        bundle = std::move(*bundle_opt_);
+                        bundle_opt_.reset();
+
+                        for (auto& p : reference_counts_by_endpoint_) {
+                            endpoints.push_back(p.first);
+                        }
+                    }
+
+                    if (endpoints.empty()) {
+                        continue;
+                    }
+
+                    ::osc::OutboundPacketStream o(reinterpret_cast<char*>(buffer_.data()),
+                                                  static_cast<unsigned long>(buffer_.size()));
+
+                    o << ::osc::BeginBundle();
+
+                    for (auto& p : bundle) {
+                        o << ::osc::BeginMessage(p.first.c_str());
+
+                        param_visitor<decltype(o)> param_visitor(o);
+                        for (const auto& element : p.second) {
+                            boost::apply_visitor(param_visitor, element);
+                        }
+
+                        o << ::osc::EndMessage;
+                    }
+
+                    o << ::osc::EndBundle;
+
+                    boost::system::error_code ec;
+                    for (const auto& endpoint : endpoints) {
+                        socket_.send_to(boost::asio::buffer(o.Data(), o.Size()), endpoint, 0, ec);
+                    }
+                }
+            } catch (...) {
+                CASPAR_LOG_CURRENT_EXCEPTION();
+            }
+        });
+    }
+
+    ~impl()
+    {
+        abort_request_ = true;
+        cond_.notify_all();
+        thread_.join();
     }
 
     // TODO (refactor) This is wierd...
     std::shared_ptr<void> get_subscription_token(const boost::asio::ip::udp::endpoint& endpoint)
     {
-        std::lock_guard<std::mutex> lock(endpoints_mutex_);
+        std::lock_guard<std::mutex> lock(mutex_);
 
         ++reference_counts_by_endpoint_[endpoint];
 
@@ -96,7 +165,7 @@ struct client::impl : public spl::enable_shared_from_this<client::impl>
 
             auto& self = *strong;
 
-            std::lock_guard<std::mutex> lock(self.endpoints_mutex_);
+            std::lock_guard<std::mutex> lock(self.mutex_);
 
             int reference_count_after = --self.reference_counts_by_endpoint_[endpoint];
 
@@ -113,40 +182,11 @@ struct client::impl : public spl::enable_shared_from_this<client::impl>
             return;
         }
 
-        std::vector<udp::endpoint> endpoints;
         {
-            std::lock_guard<std::mutex> lock(endpoints_mutex_);
-            for (auto& p : reference_counts_by_endpoint_) {
-                endpoints.push_back(p.first);
-            }
+            std::lock_guard<std::mutex> lock(mutex_);
+            bundle_opt_ = std::move(bundle);
         }
-
-        if (endpoints.empty()) {
-            return;
-        }
-
-        ::osc::OutboundPacketStream o(reinterpret_cast<char*>(buffer_.data()),
-                                      static_cast<unsigned long>(buffer_.size()));
-
-        o << ::osc::BeginBundle();
-
-        for (auto& p : bundle) {
-            o << ::osc::BeginMessage(p.first.c_str());
-
-            param_visitor<decltype(o)> param_visitor(o);
-            for (const auto& element : p.second) {
-                boost::apply_visitor(param_visitor, element);
-            }
-
-            o << ::osc::EndMessage;
-        }
-
-        o << ::osc::EndBundle;
-
-        boost::system::error_code ec;
-        for (const auto& endpoint : endpoints) {
-            socket_.send_to(boost::asio::buffer(o.Data(), o.Size()), endpoint, 0, ec);
-        }
+        cond_.notify_all();
     }
 };
 
