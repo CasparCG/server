e2cb1beeba6df1c6563582161c2187b7b48b2317
Peter Keuter <github@peterkeuter.nl>
2018-08-06 15:33:45 +0200

    Move foreground into own object

--- src/core/producer/layer.cpp
+++ src/core/producer/layer.cpp
@@ -106,11 +106,13 @@ struct layer::impl
                 frame = foreground_->last_frame();
             }
 
-            state_ = foreground_->state();
-            state_["producer"]               = foreground_->name();
+            state_ = {};
+            state_["foreground"] = foreground_->state();
+            state_["foreground"]["producer"] = foreground_->name();
+            state_["foreground"]["paused"] = paused_;
+
             state_["background"] = background_->state();
             state_["background"]["producer"] = background_->name();
-            state_["paused"] = paused_;
 
             return frame;
         } catch (...) {
