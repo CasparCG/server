05316a342951da4d880919d8af39a8632e8d3a0a
Julian Waller <git@julusian.co.uk>
2019-01-05 17:29:04 +0000

    fix(linux): Broken build

--- src/common/os/linux/thread.cpp
+++ src/common/os/linux/thread.cpp
@@ -3,6 +3,6 @@
 
 namespace caspar {
 
-void set_thread_name(std::wstring name) { pthread_setname_np(pthread_self(), u8(name).c_str()); }
+void set_thread_name(const std::wstring& name) { pthread_setname_np(pthread_self(), u8(name).c_str()); }
 
 } // namespace caspar
