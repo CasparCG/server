157ef0a33621302943ef3ae5c92bad8bbf532e93
ronag <ronag89@gmail.com>
2019-06-01 11:43:12 +0200

    fix(ffmpeg_producer): memory leak

--- src/modules/ffmpeg/producer/av_input.cpp
+++ src/modules/ffmpeg/producer/av_input.cpp
@@ -30,6 +30,7 @@ Input::Input(const std::string& filename, std::shared_ptr<diagnostics::graph> gr
     graph_->set_color("seek", diagnostics::color(1.0f, 0.5f, 0.0f));
     graph_->set_color("input", diagnostics::color(0.7f, 0.4f, 0.4f));
 
+    buffer_.set_capacity(256);
     thread_ = std::thread([=] {
         try {
             set_thread_name(L"[ffmpeg::av_producer::Input]");
