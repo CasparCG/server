72f17f31ec50d55ebc3a212dcf6c15b37592128a
ryci-us <rycius@ryci.us>
2019-06-07 10:33:21 +0300

    FFmpeg producer allways resample to 48kHz
    Fixes audio out of sync issue when playing back sources with audio sample rate other then 48kHz.
    Fixes these issues:
    https://github.com/CasparCG/server/issues/1170
    https://github.com/CasparCG/server/issues/1194

--- src/modules/ffmpeg/producer/av_producer.cpp
+++ src/modules/ffmpeg/producer/av_producer.cpp
@@ -223,7 +223,7 @@ struct Filter
                 filter_spec = "anull";
             }
 
-            filter_spec += (boost::format(",aresample=async=1000:first_pts=%d:min_comp=0.01,asetrate=r=%d,"
+            filter_spec += (boost::format(",aresample=async=1000:first_pts=%d:min_comp=0.01,aresample=%d,"
                                           "asetnsamples=n=1024:p=0") %
                             av_rescale_q(start_time, TIME_BASE_Q, {1, format_desc.audio_sample_rate}) %
                             format_desc.audio_sample_rate)
