5ca995bb7d4d37b8e4de4e9cd33033ada3b502d8
Oscar Cowdery Lack <oscar@lightningvisuals.com>
2021-10-20 22:41:18 +1100

    Fix CASPAR_THROW_EXCEPTION with newer boost
    Newer versions of boost don't define a
    BOOST_THROW_EXCEPTION_CURRENT_FUNCTION macro, and apparently it was
    never meant for user use anyway [1]. We can use BOOST_CURRENT_FUNCTION
    instead, which should be equivalent.
    
    [1]: https://github.com/boostorg/exception/issues/38

--- src/common/except.h
+++ src/common/except.h
@@ -145,7 +145,7 @@ struct not_supported : virtual user_error
 
 #define CASPAR_THROW_EXCEPTION(x)                                                                                      \
     ::boost::throw_exception(::boost::enable_error_info(x)                                                             \
-                             << ::boost::throw_function(BOOST_THROW_EXCEPTION_CURRENT_FUNCTION)                        \
+                             << ::boost::throw_function(BOOST_CURRENT_FUNCTION)                                        \
                              << ::boost::throw_file(__FILE__) << ::boost::throw_line((int)__LINE__)                    \
                              << stacktrace_info())
 
