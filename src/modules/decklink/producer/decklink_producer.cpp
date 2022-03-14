42052587a49d3e5457203b7372c728442e6b0075
Jesper Ek <jesper@boffins.se>
2022-03-14 15:22:10 +0100

    fix: upgrade ffmpeg to 5.0

--- src/modules/decklink/producer/decklink_producer.cpp
+++ src/modules/decklink/producer/decklink_producer.cpp
@@ -56,6 +56,8 @@ extern "C" {
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
 #include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/channel_layout.h>
 #include <libavutil/opt.h>
 #include <libavutil/pixfmt.h>
 #include <libavutil/samplefmt.h>
