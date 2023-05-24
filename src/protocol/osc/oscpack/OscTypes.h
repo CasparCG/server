5a011a0aa6bd7cd9c635f419b16e6e07750cacd2
Julian Waller <git@julusian.co.uk>
2023-05-24 17:36:36 +0100

    fix: simplify osc types

--- src/protocol/osc/oscpack/OscTypes.h
+++ src/protocol/osc/oscpack/OscTypes.h
@@ -35,10 +35,10 @@
 namespace osc {
 
 // basic types
-typedef int64_t  int64;
-typedef uint64_t uint64;
-typedef int32_t  int32;
-typedef uint32_t uint32;
+using int64 = std::int64_t;
+using uint64 = std::uint64_t;
+using int32 = std::int32_t;
+using uint32 = std::uint32_t;
 
 enum TypeTagValues
 {
