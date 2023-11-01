4a60eaf3070fdbb0db49fd5afa397bef4d1d2589
Julian Waller <git@julusian.co.uk>
2023-11-01 17:47:39 +0000

    chore: reduce duplication in module cmake files

--- src/CMakeModules/CasparCG_Util.cmake
+++ src/CMakeModules/CasparCG_Util.cmake
@@ -9,52 +9,78 @@ FUNCTION(casparcg_add_build_dependencies PROJECT)
 	ADD_DEPENDENCIES (${PROJECT} ${CASPARCG_EXTERNAL_PROJECTS})
 ENDFUNCTION()
 
-# Specify a module header file to build into the application
-SET (CASPARCG_MODULE_INCLUDE_STATEMENTS "" CACHE INTERNAL "")
-FUNCTION (casparcg_add_include_statement HEADER_FILE_TO_INCLUDE)
-	SET (CASPARCG_MODULE_INCLUDE_STATEMENTS "${CASPARCG_MODULE_INCLUDE_STATEMENTS}"
-			"#include <${HEADER_FILE_TO_INCLUDE}>"
-			CACHE INTERNAL ""
-	)
-ENDFUNCTION ()
-
-# Specify a module init function to call during application initialisation
-SET (CASPARCG_MODULE_INIT_STATEMENTS "" CACHE INTERNAL "")
-FUNCTION (casparcg_add_init_statement INIT_FUNCTION_NAME NAME_TO_LOG)
-	SET (CASPARCG_MODULE_INIT_STATEMENTS "${CASPARCG_MODULE_INIT_STATEMENTS}"
-			"	${INIT_FUNCTION_NAME}(dependencies)\;"
-			"	CASPAR_LOG(info) << L\"Initialized ${NAME_TO_LOG} module.\"\;"
-			""
-			CACHE INTERNAL ""
-	)
-ENDFUNCTION ()
-
-# Specify a module uninit function to call during application shutdown
-SET (CASPARCG_MODULE_UNINIT_STATEMENTS "" CACHE INTERNAL "")
-FUNCTION (casparcg_add_uninit_statement UNINIT_FUNCTION_NAME)
-	SET (CASPARCG_MODULE_UNINIT_STATEMENTS
-			"	${UNINIT_FUNCTION_NAME}()\;"
-			"${CASPARCG_MODULE_UNINIT_STATEMENTS}"
-			CACHE INTERNAL ""
-	)
-ENDFUNCTION ()
-
-# Specify a module function to call during startup to handle custom command line arguments
-SET (CASPARCG_MODULE_COMMAND_LINE_ARG_INTERCEPTORS_STATEMENTS "" CACHE INTERNAL "")
-FUNCTION (casparcg_add_command_line_arg_interceptor INTERCEPTOR_FUNCTION_NAME)
-	set(CASPARCG_MODULE_COMMAND_LINE_ARG_INTERCEPTORS_STATEMENTS "${CASPARCG_MODULE_COMMAND_LINE_ARG_INTERCEPTORS_STATEMENTS}"
-			"	if (${INTERCEPTOR_FUNCTION_NAME}(argc, argv))"
-			"		return true\;"
-			""
-			CACHE INTERNAL ""
-	)
-ENDFUNCTION ()
-
 # Add a module to be linked into the application
 SET (CASPARCG_MODULE_PROJECTS "" CACHE INTERNAL "")
 FUNCTION (casparcg_add_module_project PROJECT)
+	cmake_parse_arguments(
+        PARSED_ARGS # prefix of output variables
+        "" # list of names of the boolean arguments (only defined ones will be true)
+        "NAME;HEADER_FILE;INIT_FUNCTION;UNINIT_FUNCTION;CLI_INTERCEPTOR" # list of names of mono-valued arguments
+        "SOURCES" # list of names of multi-valued arguments (output variables are lists)
+        ${ARGN} # arguments of the function to parse, here we take the all original ones
+    )
+
+	if(NOT PROJECT)
+        message(FATAL_ERROR "You must provide a project name")
+	endif()
+
+	# Use project if name is missing
+	if (NOT PARSED_ARGS_NAME)
+		set (PARSED_ARGS_NAME ${PROJECT})
+	endif()
+	# Use default path if header not defined
+	if (NOT PARSED_ARGS_HEADER_FILE)
+		set (PARSED_ARGS_HEADER_FILE "modules/${PROJECT}/${PROJECT}.h")
+	endif()
+	# Use default init name if nto d
+	if (NOT PARSED_ARGS_INIT_FUNCTION)
+        message(FATAL_ERROR "You must provide an INIT_FUNCTION")
+	endif()
+
+	# Setup the library and some default config
+	ADD_LIBRARY (${PROJECT} ${PARSED_ARGS_SOURCES})
+	target_compile_features (${PROJECT} PRIVATE cxx_std_17)
+	target_include_directories(${PROJECT} PRIVATE
+		${BOOST_INCLUDE_PATH}
+		${TBB_INCLUDE_PATH}
+	)
+	target_link_libraries(${PROJECT} common core)
+
+	# Setup dependency on ExternalProject
+	ADD_DEPENDENCIES (${PROJECT} ${CASPARCG_EXTERNAL_PROJECTS})
+	
+	# Setup linker and code loading
 	SET (CASPARCG_MODULE_PROJECTS "${CASPARCG_MODULE_PROJECTS}" "${PROJECT}" CACHE INTERNAL "")
+	SET (CASPARCG_MODULE_INCLUDE_STATEMENTS "${CASPARCG_MODULE_INCLUDE_STATEMENTS}"
+		"#include <${PARSED_ARGS_HEADER_FILE}>"
+		CACHE INTERNAL ""
+	)
+	SET (CASPARCG_MODULE_INIT_STATEMENTS "${CASPARCG_MODULE_INIT_STATEMENTS}"
+		"	${PARSED_ARGS_INIT_FUNCTION}(dependencies)\;"
+		"	CASPAR_LOG(info) << L\"Initialized ${PARSED_ARGS_NAME} module.\"\;"
+		""
+		CACHE INTERNAL ""
+	)
+
+	IF (PARSED_ARGS_UNINIT_FUNCTION)
+		SET (CASPARCG_MODULE_UNINIT_STATEMENTS
+			"	${PARSED_ARGS_UNINIT_FUNCTION}()\;"
+			"${CASPARCG_MODULE_UNINIT_STATEMENTS}"
+			CACHE INTERNAL ""
+		)
+	ENDIF ()
+
+	IF (PARSED_ARGS_CLI_INTERCEPTOR)
+		SET (CASPARCG_MODULE_COMMAND_LINE_ARG_INTERCEPTORS_STATEMENTS "${CASPARCG_MODULE_COMMAND_LINE_ARG_INTERCEPTORS_STATEMENTS}"
+			"	if (${PARSED_ARGS_CLI_INTERCEPTOR}(argc, argv))"
+			"		return true\;"
+			""
+			CACHE INTERNAL ""
+		)
+	ENDIF ()
+  
 ENDFUNCTION ()
+
 
 # http://stackoverflow.com/questions/7172670/best-shortest-way-to-join-a-list-in-cmake
 FUNCTION (join_list VALUES GLUE OUTPUT)
