623d74942c43b22e3474b4f38435172b8755d5f4
Julian Waller <git@julusian.co.uk>
2024-01-05 16:34:23 +0000

    wip: rework frame flow

--- src/modules/html/producer/html_producer.cpp
+++ src/modules/html/producer/html_producer.cpp
@@ -82,7 +82,13 @@ struct presentation_frame
     bool               has_video = false;
     bool               has_audio = false;
 
-    explicit presentation_frame() {}
+    explicit presentation_frame(core::draw_frame video = {})
+    {
+        if (video) {
+            frame     = std::move(video);
+            has_video = true;
+        }
+    }
 
     presentation_frame(presentation_frame&& other) noexcept
         : timestamp(other.timestamp)
@@ -261,12 +267,13 @@ class html_client
     {
         if (!try_pop(field)) {
             graph_->set_tag(diagnostics::tag_severity::SILENT, "late-frame");
+            return core::draw_frame::still(last_frame_);
+        } else {
+            return last_frame_;
         }
-
-        return last_frame_;
     }
 
-    core::draw_frame last_frame() const { return last_frame_; }
+    core::draw_frame last_frame() const { return core::draw_frame::still(last_frame_); }
 
     bool is_ready() const
     {
@@ -370,10 +377,7 @@ class html_client
             core::draw_frame new_frame = core::draw_frame(std::move(frame));
             last_generated_frame_      = new_frame;
 
-            presentation_frame wrapped_frame;
-            wrapped_frame.add_video(std::move(new_frame));
-
-            frames_.push(std::move(wrapped_frame));
+            frames_.push(presentation_frame(std::move(new_frame)));
             while (frames_.size() > 4) {
                 frames_.pop();
                 graph_->set_tag(diagnostics::tag_severity::WARNING, "dropped-frame");
@@ -520,19 +524,15 @@ class html_client
         {
             std::lock_guard<std::mutex> lock(frames_mutex_);
             if (frames_.empty()) {
-                presentation_frame wrapped_frame;
-
+                presentation_frame wrapped_frame(last_generated_frame_);
                 wrapped_frame.add_audio(std::move(audio_frame));
-                if (last_generated_frame_) {
-                    wrapped_frame.add_video(last_generated_frame_);
-                }
 
                 frames_.push(std::move(wrapped_frame));
             } else {
                 if (!frames_.back().has_audio) {
                     frames_.back().add_audio(std::move(audio_frame));
                 } else {
-                    presentation_frame wrapped_frame;
+                    presentation_frame wrapped_frame(last_generated_frame_);
                     wrapped_frame.add_audio(std::move(audio_frame));
                     frames_.push(std::move(wrapped_frame));
                 }
