66816a697d54b209802437f00c546d66fc1a847c
Oscar Cowdery Lack <oscar@lightningvisuals.com>
2024-01-17 11:19:02 +1100

    Allow configuring diag font path at build time
    This allows for better integration with distros that put the system font
    in a different directory.

--- src/core/diagnostics/osd_graph.cpp
+++ src/core/diagnostics/osd_graph.cpp
@@ -66,14 +66,8 @@ sf::Font& get_default_font()
 {
     static sf::Font DEFAULT_FONT = []() {
         sf::Font font;
-#ifdef USE_SYSTEM_DIAG_FONT
-        if (!font.loadFromFile("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf"))
-            CASPAR_THROW_EXCEPTION(file_not_found() << msg_info(
-                                       "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf not found"));
-#else
-        if (!font.loadFromFile("LiberationMono-Regular.ttf"))
-            CASPAR_THROW_EXCEPTION(file_not_found() << msg_info("LiberationMono-Regular.ttf not found"));
-#endif
+        if (!font.loadFromFile(DIAG_FONT_PATH))
+            CASPAR_THROW_EXCEPTION(file_not_found() << msg_info(DIAG_FONT_PATH " not found"));
         return font;
     }();
 
