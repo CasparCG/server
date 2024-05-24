23e9039366370accd868f0debc55089f27d45fb3
Julian Waller <git@julusian.co.uk>
2024-05-24 14:37:15 +0100

    Revert "fix: replace tbbmalloc with os native implementations"
    tbbmalloc is necessary on windows to get satisfactory performance in many cases #1537
    
    This reverts commit 3816e08f5b89cd1139a4c46a47bb1aef2b6b15f4.

--- src/common/memshfl.h
+++ src/common/memshfl.h
@@ -26,6 +26,7 @@
 #include <simde/x86/ssse3.h>
 #else
 #ifdef _MSC_VER
+#include <tbb/scalable_allocator.h>
 #include <intrin.h>
 #else
 #include <tmmintrin.h>
@@ -37,7 +38,7 @@ namespace caspar {
 #ifdef _MSC_VER
 static std::shared_ptr<void> create_aligned_buffer(size_t size)
 {
-    return std::shared_ptr<void>(_aligned_malloc(size, 64), _aligned_free);
+    return std::shared_ptr<void>(scalable_aligned_malloc(size, 64), scalable_aligned_free);
 }
 #else
 static std::shared_ptr<void> create_aligned_buffer(size_t size)
