b027fcb33f03646bb6b0ba61c8d982770444db54
Julian Waller <git@julusian.co.uk>
2025-01-12 16:31:30 +0000

    fix: flush logs before exit #1571

--- src/shell/main.cpp
+++ src/shell/main.cpp
@@ -283,5 +283,7 @@ int main(int argc, char** argv)
         std::this_thread::sleep_for(std::chrono::milliseconds(4000));
     }
 
+    boost::log::core::get()->flush();
+
     return return_code;
 }
