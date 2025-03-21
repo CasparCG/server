d17477c6f929b8e434265b9a6c0c8843ff43ad4c
Niklas Andersson <3985238+niklaspandersson@users.noreply.github.com>
2025-03-21 16:11:42 +0100

    amcp: add APPLY command, similar to CALL but for consumers

--- src/core/consumer/frame_consumer.h
+++ src/core/consumer/frame_consumer.h
@@ -27,6 +27,7 @@
 #include <common/bit_depth.h>
 #include <common/memory.h>
 
+#include <common/future.h>
 #include <core/video_format.h>
 
 #include <boost/property_tree/ptree_fwd.hpp>
@@ -49,8 +50,10 @@ class frame_consumer
     frame_consumer() {}
     virtual ~frame_consumer() {}
 
-    virtual std::future<bool> send(const core::video_field field, const_frame frame)              = 0;
-    virtual void              initialize(const video_format_desc& format_desc, const core::channel_info& channel_info, int port_index) = 0;
+    virtual std::future<bool> send(const core::video_field field, const_frame frame) = 0;
+    virtual void
+    initialize(const video_format_desc& format_desc, const core::channel_info& channel_info, int port_index) = 0;
+    virtual std::future<bool> call(const std::vector<std::wstring>& params) { return caspar::make_ready_future(false); }
 
     virtual core::monitor::state state() const = 0;
 
