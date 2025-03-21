e35ff1c09e44e35aa0d98a31086d9340f4650090
Niklas Andersson <3985238+niklaspandersson@users.noreply.github.com>
2025-03-21 16:11:42 +0100

    feat: support 'call' commands to consumers ('apply')

--- src/core/consumer/frame_consumer.h
+++ src/core/consumer/frame_consumer.h
@@ -51,6 +51,12 @@ class frame_consumer
 
     virtual std::future<bool> send(const core::video_field field, const_frame frame)              = 0;
     virtual void              initialize(const video_format_desc& format_desc, const core::channel_info& channel_info, int port_index) = 0;
+    virtual std::future<bool> call(const std::vector<std::wstring>& params) const
+    {
+        std::promise<bool> p;
+        p.set_value(false);
+        return p.get_future();
+    }
 
     virtual core::monitor::state state() const = 0;
 
