25380774b276559746f1675244252cfced3094a1
Julian Waller <git@julusian.co.uk>
2025-12-10 16:43:39 +0000

    chore: fix typo

--- src/modules/decklink/consumer/config.cpp
+++ src/modules/decklink/consumer/config.cpp
@@ -125,7 +125,7 @@ configuration parse_xml_config(const boost::property_tree::wptree&  ptree,
         if (!CHECK_INSTRUCTION_SUPPORT(__IA_SUPPORT_VECTOR256, 0) &&
             config.pixel_format != configuration::pixel_format_t::rgba) {
             CASPAR_THROW_EXCEPTION(user_error()
-                                   << msg_info(L"You cpu does not support the features needed for yuv output"));
+                                   << msg_info(L"Your cpu does not support the features needed for yuv output"));
         }
 #endif
     }
