af310f1cde281e0566266b89b5b0083725b72518
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-05-20 20:46:48 -0400

    chore(endian): modernize

--- src/common/endian.h
+++ src/common/endian.h
@@ -18,55 +18,84 @@
  *
  * Author: Helge Norberg, helge.norberg@svt.se
  * Author: Robert Nagy, ronag89@gmail.com
+ * Author: Dimitry Ishenko, dimitry (dot) ishenko (at) (gee) mail (dot) com
  */
 
 #pragma once
 
-#include <cstdint>
+#include <concepts>
+#include <cstddef> // std::size_t
 #include <type_traits>
+
+#if defined(_MSC_VER)
+#include <stdlib.h> // _byteswap_...
+#endif
+
+template <typename T, std::size_t N>
+concept integral_of_size = std::integral<T> && (sizeof(T) == N);
 
 namespace caspar {
 
-template <typename T>
-typename std::enable_if<sizeof(T) == sizeof(std::uint8_t), T>::type swap_byte_order(const T& value)
-{
-    return value;
-}
+template <integral_of_size<1> V>
+constexpr auto swap_byte_order(V val) { return val; }
 
-template <typename T>
-typename std::enable_if<sizeof(T) == sizeof(std::uint16_t), T>::type swap_byte_order(const T& value)
+template <integral_of_size<2> V>
+constexpr auto swap_byte_order(V val)
 {
-#ifdef _MSC_VER
-    auto swapped = _byteswap_ushort(reinterpret_cast<const unsigned short&>(value));
-#elif __GNUC__
-    auto swapped = __builtin_bswap16(value);
+    auto uval = static_cast<std::make_unsigned_t<V>>(val);
+
+#if defined(_MSC_VER)
+    auto result = _byteswap_ushort(uval);
+#elif defined(__GNUC__) || defined(__clang__)
+    auto result = __builtin_bswap16(uval);
+#else
+    auto result = ((uval & 0x00ffu) << 8) |
+                  ((uval & 0xff00u) >> 8);
 #endif
 
-    return reinterpret_cast<const T&>(swapped);
+    return static_cast<V>(result);
 }
 
-template <typename T>
-typename std::enable_if<sizeof(T) == sizeof(std::uint32_t), T>::type swap_byte_order(const T& value)
+template <integral_of_size<4> V>
+constexpr auto swap_byte_order(V val)
 {
-#ifdef _MSC_VER
-    auto swapped = _byteswap_ulong(reinterpret_cast<const unsigned long&>(value));
-#elif __GNUC__
-    auto swapped = __builtin_bswap32(value);
+    auto uval = static_cast<std::make_unsigned_t<V>>(val);
+
+#if defined(_MSC_VER)
+    auto result = _byteswap_ulong(uval);
+#elif defined(__GNUC__) || defined(__clang__)
+    auto result = __builtin_bswap32(uval);
+#else
+    auto result = ((uval & 0x000000ffu) << 24) |
+                  ((uval & 0x0000ff00u) <<  8) |
+                  ((uval & 0x00ff0000u) >>  8) |
+                  ((uval & 0xff000000u) >> 24);
 #endif
 
-    return reinterpret_cast<const T&>(swapped);
+    return static_cast<V>(result);
 }
 
-template <typename T>
-typename std::enable_if<sizeof(T) == sizeof(std::uint64_t), T>::type swap_byte_order(const T& value)
+template <integral_of_size<8> V>
+constexpr auto swap_byte_order(V val)
 {
-#ifdef _MSC_VER
-    auto swapped = _byteswap_uint64(reinterpret_cast<const unsigned long long&>(value));
-#elif __GNUC__
-    auto swapped = __builtin_bswap64(value);
+    auto uval = static_cast<std::make_unsigned_t<V>>(val);
+
+#if defined(_MSC_VER)
+    auto result = _byteswap_uint64(uval);
+#elif defined(__GNUC__) || defined(__clang__)
+    auto result = __builtin_bswap64(uval);
+#else
+    auto result = ((uval & 0x00000000000000ffull) << 56) |
+                  ((uval & 0x000000000000ff00ull) << 40) |
+                  ((uval & 0x0000000000ff0000ull) << 24) |
+                  ((uval & 0x00000000ff000000ull) <<  8) |
+                  ((uval & 0x000000ff00000000ull) >>  8) |
+                  ((uval & 0x0000ff0000000000ull) >> 24) |
+                  ((uval & 0x00ff000000000000ull) >> 40) |
+                  ((uval & 0xff00000000000000ull) >> 56);
 #endif
 
-    return reinterpret_cast<const T&>(swapped);
+    return static_cast<V>(result);
 }
 
 } // namespace caspar
