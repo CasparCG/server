dedicatedbroadcastsolutions/casparcg-server

add side_data_queue argument to ffmpeg::make_frame

src/modules/ffmpeg/util/av_util.h
@@ -10,6 +10,7 @@ extern "C" {
 
 #include <core/frame/frame.h>
 #include <core/frame/frame_factory.h>
+#include <core/frame/frame_side_data.h>
 #include <core/frame/geometry.h>
 #include <core/frame/pixel_format.h>
 #include <core/video_format.h>
@@ -40,13 +41,14 @@ core::pixel_format_desc pixel_format_desc(AVPixelFormat     pix_fmt,
                                           int               height,
                                           std::vector<int>& data_map,
                                           core::color_space color_space = core::color_space::bt709);
-core::mutable_frame     make_frame(void*                    tag,
-                                   core::frame_factory&     frame_factory,
-                                   std::shared_ptr<AVFrame> video,
-                                   std::shared_ptr<AVFrame> audio,
-                                   core::color_space        color_space = core::color_space::bt709,
-                                   core::frame_geometry::scale_mode     = core::frame_geometry::scale_mode::stretch,
-                                   bool is_straight_alpha               = false);
+core::mutable_frame     make_frame(void*                                        tag,
+                                   core::frame_factory&                         frame_factory,
+                                   std::shared_ptr<AVFrame>                     video,
+                                   std::shared_ptr<AVFrame>                     audio,
+                                   std::shared_ptr<core::frame_side_data_queue> side_data_queue,
+                                   core::color_space                            color_space = core::color_space::bt709,
+                                   core::frame_geometry::scale_mode = core::frame_geometry::scale_mode::stretch,
+                                   bool is_straight_alpha           = false);
 
 std::shared_ptr<AVFrame> make_av_video_frame(const core::const_frame& frame, const core::video_format_desc& format_des);
 std::shared_ptr<AVFrame> make_av_audio_frame(const core::const_frame& frame, const core::video_format_desc& format_des);

CasparCG/server

src/modules/ffmpeg/util/av_util.h
@@ -6,6 +6,7 @@ extern "C" {
 
 #include <core/frame/frame.h>
 #include <core/frame/frame_factory.h>
+#include <core/frame/frame_side_data.h>
 #include <core/frame/geometry.h>
 #include <core/frame/pixel_format.h>
 #include <core/video_format.h>
@@ -32,13 +33,26 @@ core::pixel_format_desc pixel_format_desc(AVPixelFormat     pix_fmt,
                                           int               height,
                                           std::vector<int>& data_map,
                                           core::color_space color_space = core::color_space::unknown);
-core::mutable_frame     make_frame(void*                    tag,
-                                   core::frame_factory&     frame_factory,
-                                   std::shared_ptr<AVFrame> video,
-                                   std::shared_ptr<AVFrame> audio,
-                                   core::color_space        color_space = core::color_space::unknown,
-                                   core::frame_geometry::scale_mode     = core::frame_geometry::scale_mode::stretch,
-                                   bool is_straight_alpha               = false);
+core::mutable_frame     make_frame(void*                                        tag,
+                                   core::frame_factory&                         frame_factory,
+                                   std::shared_ptr<AVFrame>                     video,
+                                   std::shared_ptr<AVFrame>                     audio,
+                                   std::shared_ptr<core::frame_side_data_queue> side_data_queue,
+                                   core::color_space                            color_space = core::color_space::unknown,
+                                   core::frame_geometry::scale_mode = core::frame_geometry::scale_mode::stretch,
+                                   bool is_straight_alpha           = false);
+inline core::mutable_frame
+make_frame(void*                            tag,
+           core::frame_factory&             frame_factory,
+           std::shared_ptr<AVFrame>         video,
+           std::shared_ptr<AVFrame>         audio,
+           core::color_space                color_space       = core::color_space::unknown,
+           core::frame_geometry::scale_mode scale_mode        = core::frame_geometry::scale_mode::stretch,
+           bool                             is_straight_alpha = false)
+{
+    return make_frame(
+        tag, frame_factory, std::move(video), std::move(audio), nullptr, color_space, scale_mode, is_straight_alpha);
+}
 
 std::shared_ptr<AVFrame> make_av_video_frame(const core::const_frame& frame, const core::video_format_desc& format_des);
 std::shared_ptr<AVFrame> make_av_audio_frame(const core::const_frame& frame, const core::video_format_desc& format_des);